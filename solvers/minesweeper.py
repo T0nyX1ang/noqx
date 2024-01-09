1104a6059356814b9b9414831911aa82b8a60bfe
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2024-01-10 01:23:24 +0800

    style: change some rule constructions

--- solvers/minesweeper.py
+++ solvers/minesweeper.py
@@ -4,7 +4,14 @@ from typing import Dict, List
 
 from . import utilsx
 from .utilsx.encoding import Encoding
-from .utilsx.rules import adjacent, display, grid, ranged, shading
+from .utilsx.rules import (
+    diag_adjacent,
+    display,
+    grid,
+    orth_adjacent,
+    ranged,
+    shade_without_num,
+)
 from .utilsx.solutions import solver
 
 
@@ -16,9 +23,10 @@ def solve(E: Encoding) -> List[Dict[str, str]]:
     solver.reset()
     solver.add_program_line(grid(E.R, E.C))
     solver.add_program_line(ranged(0, 8))
-    solver.add_program_line(shading())
-    solver.add_program_line(adjacent())
-    solver.add_program_line("N {black(R1, C1) : adj(R, C, R1, C1)} N :- number(R, C, N).")
+    solver.add_program_line(shade_without_num())
+    solver.add_program_line(orth_adjacent())
+    solver.add_program_line(diag_adjacent())
+    solver.add_program_line("{black(R1, C1) : adj(R, C, R1, C1)} = N :- number(R, C, N).")
 
     for (r, c), num in E.clues.items():
         if num != "?":
--- solvers/utilsx/rules.py
+++ solvers/utilsx/rules.py
@@ -16,47 +16,59 @@ def grid(rows: int, cols: int) -> str:
     return f"grid(0..{rows - 1}, 0..{cols - 1})."
 
 
-def shading(name: str = "range") -> str:
-    """Generates a rule that enforces a black cell or a with cell with ranged numbers."""
-    return f"1 {{number(R, C, N) : {name}(N) ; black(R, C)}} 1 :- grid(R, C)."
+def shade(color: str = "black") -> str:
+    """
+    Generate a rule that a cell is either {color} or not {color}.
+
+    A grid rule should be defined first."""
+    return f"{{{color}(R, C)}} :- grid(R, C)."
 
 
-def hv_adjacent() -> str:
-    """Generates a rule for getting the horizontal/vertical neighbors."""
+def shade_without_num(name: str = "range", color: str = "black") -> str:
+    """Generates a rule that enforces a {color} cell or a with cell with ranged numbers."""
+    return f"{{number(R, C, N) : {name}(N) ; {color}(R, C)}} = 1 :- grid(R, C)."
+
+
+def orth_adjacent() -> str:
+    """
+    Generates a rule for getting the orthogonal neighbors.
+
+    A grid rule should be defined first.
+    """
     return "adj(R, C, R1, C1) :- grid(R, C), grid(R1, C1), |R - R1| + |C - C1| == 1."
 
 
 def diag_adjacent() -> str:
-    """Generates a rule for getting the diagonal neighbors."""
+    """
+    Generates a rule for getting the diagonal neighbors.
+
+    A grid rule should be defined first.
+    """
     return "adj(R, C, R1, C1) :- grid(R, C), grid(R1, C1), |R - R1| == 1, |C - C1| == 1."
 
 
-def adjacent() -> str:
-    """Generates a rule for getting the surroundings neighbors."""
-    hv_rule = hv_adjacent()
-    diag_rule = diag_adjacent()
-    return f"{hv_rule}\n{diag_rule}"
+def avoid_adjacent(color: str = "black") -> str:
+    """
+    Generates a constraint to avoid adjacent {color} cells based on adjacent definition.
+
+    An adjacent rule should be defined first.
+    """
+    return f":- {color}(R, C), {color}(R1, C1), adj(R, C, R1, C1)."
 
 
-def no_adjacent(color: str = "black") -> str:
-    """Generates a rule for no adjacent {color: default = black} cells."""
-    adj_rule = adjacent()
-    avoid_rule = f":- {color}(R, C), {color}(R1, C1), adj(R, C, R1, C1)."
-    return f"{adj_rule}\n{avoid_rule}"
+def row_num_unique(color: str = "black") -> str:
+    """
+    Generates a constraint for unique {colo} numbered cell in every row.
 
-
-def r_unique(color: str = "-black") -> str:
-    """Generates a rule for unique {color: default = not black} cell in every row."""
+    A number rule should be defined first.
+    """
     return f":- number(_, C, N), 2 {{ {color}(R, C) : number(R, C, N) }}."
 
 
-def c_unique(color: str = "-black") -> str:
-    """Generates a rule for unique {color: default = not black} cell in every row."""
+def col_num_unique(color: str = "black") -> str:
+    """
+    Generates a constraint for unique {color} numbered cell in every column.
+
+    A number rule should be defined first.
+    """
     return f":- number(R, _, N), 2 {{ {color}(R, C) : number(R, C, N) }}."
-
-
-def rc_unique(color: str = "-black") -> str:
-    """Generates a rule for unique {color: default = not black} cell in every row and column."""
-    r_uni_rule = r_unique(color)
-    c_uni_rule = c_unique(color)
-    return f"{r_uni_rule}\n{c_uni_rule}"
