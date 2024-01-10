8f21888b344a26d3c84e0bb1c2e95da33baafd64
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2024-01-10 23:58:37 +0800

    feat: add a rule/constraint to check connectivity

--- solvers/utilsx/rules.py
+++ solvers/utilsx/rules.py
@@ -72,3 +72,19 @@ def col_num_unique(color: str = "black") -> str:
     A number rule should be defined first.
     """
     return f":- number(R, _, N), 2 {{ {color}(R, C) : number(R, C, N) }}."
+
+
+def connected(color: str = "black") -> str:
+    """
+    Generate a rule and a constraint to check the connectivity of {color} cells.
+
+    An adjacent rule and a grid rule should be defined first.
+    """
+
+    color_escape = color.replace("-", "_").replace(" ", "_")  # make a valid predicate name
+    reachable = f"reachable_{color_escape}(R, C) :- (R, C) = #min{{(R1, C1) : {color}(R1, C1), grid(R1, C1)}}."
+    reachable_propagation = (
+        f"reachable_{color_escape}(R, C) :- reachable_{color_escape}(R1, C1), adj(R, C, R1, C1), {color}(R, C)."
+    )
+    connectivity = f":- grid(R, C), {color}(R, C), not reachable_{color_escape}(R, C)."
+    return "\n".join([reachable, reachable_propagation, connectivity])
