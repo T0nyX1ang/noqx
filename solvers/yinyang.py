b0f5114ce2f2088d9140bec3577087b624de003d
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2024-01-11 21:43:42 +0800

    feat: upgrade the yin-yang solver

--- solvers/yinyang.py
+++ solvers/yinyang.py
@@ -2,45 +2,41 @@
 
 from typing import List
 
-from . import utils
-from .utils.claspy import require, set_max_val
-from .utils.encoding import Encoding
-from .utils.solutions import get_all_grid_solutions
-from .utils.shading import RectangularGridShadingSolver
+from . import utilsx
+from .utilsx.encoding import Encoding
+from .utilsx.rules import avoid_2x2, connected, display, grid, orth_adjacent, shade_cc
+from .utilsx.solutions import solver
 
 
 def encode(string: str) -> Encoding:
-    return utils.encode(string, clue_encoder=lambda s: s)
+    return utilsx.encode(string, clue_encoder=lambda s: s)
 
 
 def solve(E: Encoding) -> List:
-    set_max_val(2)
+    solver.reset()
+    solver.add_program_line(grid(E.R, E.C))
+    solver.add_program_line(shade_cc(color1="black", color2="white"))
+    solver.add_program_line(orth_adjacent())
+    solver.add_program_line(connected(color="black"))
+    solver.add_program_line(avoid_2x2(color="black"))
+    solver.add_program_line(connected(color="white"))
+    solver.add_program_line(avoid_2x2(color="white"))
 
-    s = RectangularGridShadingSolver(E.R, E.C)
+    for (r, c), color in E.clues.items():
+        color = "black" if color == "b" else "white"
+        solver.add_program_line(f"{color}({r}, {c}).")
 
-    # Optimize solving by providing known roots for white and black parts
-    white_root, black_root = None, None
-    for r, c in E.clues:
-        if E.clues[(r, c)] == "w":
-            white_root = (r, c)
-        else:
-            black_root = (r, c)
-        if white_root and black_root:
-            break
+    solver.add_program_line(display(color="black"))
+    solver.add_program_line(display(color="white"))
+    solver.solve()
 
-    s.white_connectivity(white_root)
-    s.black_connectivity(black_root)
-    s.no_white_2x2()
-    s.no_black_2x2()
+    for solution in solver.solutions:
+        for rc, color in solution.items():
+            color = "black" if color == "1" else "white"
+            solution[rc] += "_circle.png"
 
-    for r, c in E.clues:
-        require(s.grid[r][c] == (E.clues[(r, c)] == "b"))
-
-    def format_function(r, c):
-        return ("black" if s.grid[r][c].value() else "white") + "_circle.png"
-
-    return get_all_grid_solutions(s.grid, format_function=format_function)
+    return solver.solutions
 
 
 def decode(solutions: List[Encoding]) -> str:
-    return utils.decode(solutions)
+    return utilsx.decode(solutions)
