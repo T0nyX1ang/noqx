95a4aa36153b25e2f2a4a56e4bdf1c6bfebcc791
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2024-02-01 23:53:28 +0800

    refactor: code simplification for nonogram

--- solvers/nonogram.py
+++ solvers/nonogram.py
@@ -1,6 +1,6 @@
 """The Nonogram solver."""
 
-from typing import List, Union
+from typing import Dict, List, Union, Tuple
 
 from . import utilsx
 from .utilsx.encoding import Encoding
@@ -13,81 +13,100 @@ def encode(string: str) -> Encoding:
     return utilsx.encode(string, clue_encoder=lambda s: s, outside_clues="1001")
 
 
-def nono_row(R: int, C: int, clues: list[list[Union[int, str]]], color: str = "black"):
+def nono_row(R: int, C: int, clues: Dict[int, Tuple[Union[int, str]]], color: str = "black"):
     # Take care that start and end is left-close-right-open.
-    tag = "row"
-    init = f"{{ {tag}_start(R, C) }} :- grid(R, C).\n"
-    init += f"{tag}_endgrid(0..{R-1}, 0..{C}).\n"
-    init += f"{{ {tag}_end(R, C) }} :- {tag}_endgrid(R, C)."
-    constraints = [init]
+    constraints = []
+    constraints.append(f"row_endgrid(0..{R - 1}, 0..{C}).")
+    constraints.append("{ row_start(R, C) } :- grid(R, C).")
+    constraints.append("{ row_end(R, C) } :- row_endgrid(R, C).")
+
     for i, clue in clues.items():
-        cell_str = f"{i}, C"
         if len(clue) == 1 and clue[0] == 0:
-            constraint_i = f":- grid({cell_str}), {color}({cell_str}).\n"
-            constraint_i += f":- grid({cell_str}), {tag}_start({cell_str}).\n"
-            constraint_i += f":- 0 <= C, C < {C+1}, {tag}_end({cell_str})."
+            constraints.append(f":- grid({i}, C), {color}({i}, C).")
+            constraints.append(f":- grid({i}, C), row_start({i}, C).")
+            constraints.append(f":- row_endgrid({i}, C), row_end({i}, C).")
         else:
-            constraint_i = f":- grid(0,0), not {tag}_start_count({i}, {C-1}, {len(clue)})."
-            constraint_i += f"\n:- grid(0,0), not {tag}_end_count({i}, {C}, {len(clue)})."
+            constraints.append(f"row_count_range({i}, 0..{len(clue)}).")
+            constraints.append(f":- not row_start_count({i}, {C - 1}, {len(clue)}).")
+            constraints.append(f":- not row_end_count({i}, {C}, {len(clue)}).")
             for j, num in enumerate(clue):
                 if num != "?":
-                    constraint_i += f"\n:- grid({cell_str}), {tag}_start({cell_str}), {tag}_start_count({cell_str}, {j+1}), not {tag}_end({i}, C+{num})."
-                    constraint_i += f"\n:- grid({cell_str}), {tag}_start({cell_str}), {tag}_start_count({cell_str}, {j+1}), not {tag}_start_count({i}, C+{num-1}, {j+1})."
-            constraint_i += f"\n{tag}_count_range({i}, 0..{len(clue)})."
-            constraint_i += f"\n:- not {color}({cell_str}), 0<=N, N<={len(clue)}, {tag}_start_count({cell_str}, N + 1), {tag}_end_count({cell_str}, N)."
-            constraint_i += f"\n:- {color}({cell_str}), 0<=N, N<={len(clue)}, {tag}_start_count({cell_str}, N), {tag}_end_count({cell_str}, N)."
-        constraints.append(constraint_i)
+                    constraints.append(
+                        f":- grid({i}, C), row_start({i}, C), row_start_count({i}, C, {j + 1}), not row_end({i}, C + {num})."
+                    )
+                    constraints.append(
+                        f":- grid({i}, C), row_start({i}, C), row_start_count({i}, C, {j + 1}), not row_start_count({i}, C + {num - 1}, {j + 1})."
+                    )
 
-    constraint = f"{tag}_start_count(R, -1, 0) :- grid(R, _).\n"
-    constraint += f"{tag}_start_count(R, C, N) :- grid(R, C), {tag}_count_range(R, N), {tag}_start(R, C), {tag}_start_count(R, C-1, N-1).\n"
-    constraint += f"{tag}_start_count(R, C, N) :- grid(R, C), {tag}_count_range(R, N), not {tag}_start(R, C), {tag}_start_count(R, C-1, N).\n"
-    constraint += f"{tag}_end_count(R, -1, 0) :- grid(R, _).\n"
-    constraint += f"{tag}_end_count(R, C, N) :- {tag}_endgrid(R, C), {tag}_count_range(R, N), {tag}_end(R, C), {tag}_end_count(R, C-1, N-1).\n"
-    constraint += f"{tag}_end_count(R, C, N) :- {tag}_endgrid(R, C), {tag}_count_range(R, N), not {tag}_end(R, C), {tag}_end_count(R, C-1, N).\n"
-    constraint += f":- {tag}_start_count(R, C, N1), {tag}_end_count(R, C, N2), N1 > N2 + 1.\n"
-    constraint += f":- {tag}_start_count(R, C, N1), {tag}_end_count(R, C, N2), N1 < N2.\n"
-    constraint += f":- grid(R, C), {tag}_end(R, C), {tag}_start(R, C)."
-    constraints.append(constraint)
-    
+    constraints.append(f":- grid(R, C), not {color}(R, C), row_start_count(R, C, N + 1), row_end_count(R, C, N).")
+    constraints.append(f":- grid(R, C), {color}(R, C), row_start_count(R, C, N), row_end_count(R, C, N).")
+
+    constraints.append("row_start_count(R, -1, 0) :- grid(R, _).")
+    constraints.append(
+        "row_start_count(R, C, N) :- grid(R, C), row_count_range(R, N), row_start(R, C), row_start_count(R, C - 1, N - 1)."
+    )
+    constraints.append(
+        "row_start_count(R, C, N) :- grid(R, C), row_count_range(R, N), not row_start(R, C), row_start_count(R, C - 1, N)."
+    )
+    constraints.append("row_end_count(R, -1, 0) :- grid(R, _).")
+    constraints.append(
+        "row_end_count(R, C, N) :- row_endgrid(R, C), row_count_range(R, N), row_end(R, C), row_end_count(R, C - 1, N - 1)."
+    )
+    constraints.append(
+        "row_end_count(R, C, N) :- row_endgrid(R, C), row_count_range(R, N), not row_end(R, C), row_end_count(R, C - 1, N)."
+    )
+    constraints.append(":- row_start_count(R, C, N1), row_end_count(R, C, N2), N1 > N2 + 1.")
+    constraints.append(":- row_start_count(R, C, N1), row_end_count(R, C, N2), N1 < N2.")
+    constraints.append(":- grid(R, C), row_end(R, C), row_start(R, C).")
+
     return "\n".join(constraints)
 
 
-def nono_col(R: int, C: int, clues: list[list[Union[int, str]]], color: str = "black"):
+def nono_col(R: int, C: int, clues: Dict[int, Tuple[Union[int, str]]], color: str = "black"):
     # Take care that start and end is left-close-right-open.
-    tag = "col"
-    init = f"{{ {tag}_start(R, C) }} :- grid(R, C).\n"
-    init += f"{tag}_endgrid(0..{R}, 0..{C-1}).\n"
-    init += f"{{ {tag}_end(R, C) }} :- {tag}_endgrid(R, C).\n"
-    constraints = [init]
+    constraints = []
+    constraints.append("{ col_start(R, C) } :- grid(R, C).")
+    constraints.append(f"col_endgrid(0..{R}, 0..{C - 1}).")
+    constraints.append("{ col_end(R, C) } :- col_endgrid(R, C).")
+
     for i, clue in clues.items():
-        cell_str = f"R, {i}"
         if len(clue) == 1 and clue[0] == 0:
-            constraint_i = f":- grid({cell_str}), {color}({cell_str}).\n"
-            constraint_i += f":- grid({cell_str}), {tag}_start({cell_str}).\n"
-            constraint_i += f":- 0 <= R, R < {R+1}, {tag}_end({cell_str})."
+            constraints.append(f":- grid(R, {i}), {color}(R, {i}).")
+            constraints.append(f":- grid(R, {i}), col_start(R, {i}).")
+            constraints.append(f":- col_endgrid(R, {i}), col_end(R, {i}).")
         else:
-            constraint_i = f":- grid(0,0), not {tag}_start_count({R-1}, {i}, {len(clue)})."
-            constraint_i += f"\n:- grid(0,0), not {tag}_end_count({R}, {i}, {len(clue)})."
+            constraints.append(f":- not col_start_count({R - 1}, {i}, {len(clue)}).")
+            constraints.append(f":- not col_end_count({R}, {i}, {len(clue)}).")
+            constraints.append(f"col_count_range({i}, 0..{len(clue)}).")
             for j, num in enumerate(clue):
                 if num != "?":
-                    constraint_i += f"\n:- grid({cell_str}), {tag}_start({cell_str}), {tag}_start_count({cell_str}, {j+1}), not {tag}_end(R+{num}, {i})."
-                    constraint_i += f"\n:- grid({cell_str}), {tag}_start({cell_str}), {tag}_start_count({cell_str}, {j+1}), not {tag}_start_count(R+{num-1}, {i}, {j+1})."
-            constraint_i += f"\n{tag}_count_range({i}, 0..{len(clue)})."
-            constraint_i += f"\n:- not {color}({cell_str}), 0<=N, N<={len(clue)}, {tag}_start_count({cell_str}, N + 1), {tag}_end_count({cell_str}, N)."
-            constraint_i += f"\n:- {color}({cell_str}), 0<=N, N<={len(clue)}, {tag}_start_count({cell_str}, N), {tag}_end_count({cell_str}, N)."
-        constraints.append(constraint_i)
+                    constraints.append(
+                        f":- grid(R, {i}), col_start(R, {i}), col_start_count(R, {i}, {j + 1}), not col_end(R + {num}, {i})."
+                    )
+                    constraints.append(
+                        f":- grid(R, {i}), col_start(R, {i}), col_start_count(R, {i}, {j + 1}), not col_start_count(R + {num - 1}, {i}, {j + 1})."
+                    )
 
-    constraint = f"{tag}_start_count(-1, C, 0) :- grid(_, C).\n"
-    constraint += f"{tag}_start_count(R, C, N) :- grid(R, C), {tag}_count_range(C, N), {tag}_start(R, C), {tag}_start_count(R-1, C, N-1).\n"
-    constraint += f"{tag}_start_count(R, C, N) :- grid(R, C), {tag}_count_range(C, N), not {tag}_start(R, C), {tag}_start_count(R-1, C, N).\n"
-    constraint += f"{tag}_end_count(-1, C, 0) :- grid(_, C).\n"
-    constraint += f"{tag}_end_count(R, C, N) :- {tag}_endgrid(R, C), {tag}_count_range(C, N), {tag}_end(R, C), {tag}_end_count(R-1, C, N-1).\n"
-    constraint += f"{tag}_end_count(R, C, N) :- {tag}_endgrid(R, C), {tag}_count_range(C, N), not {tag}_end(R, C), {tag}_end_count(R-1, C, N).\n"
-    constraint += f":- {tag}_start_count(R, C, N1), {tag}_end_count(R, C, N2), N1 > N2 + 1.\n"
-    constraint += f":- {tag}_start_count(R, C, N1), {tag}_end_count(R, C, N2), N1 < N2.\n"
-    constraint += f":- grid(R, C), {tag}_end(R, C), {tag}_start(R, C)."
-    constraints.append(constraint)
-    
+    constraints.append(f":- grid(R, C), not {color}(R, C), col_start_count(R, C, N + 1), col_end_count(R, C, N).")
+    constraints.append(f":- grid(R, C), {color}(R, C), col_start_count(R, C, N), col_end_count(R, C, N).")
+    constraints.append("col_start_count(-1, C, 0) :- grid(_, C).")
+    constraints.append(
+        "col_start_count(R, C, N) :- grid(R, C), col_count_range(C, N), col_start(R, C), col_start_count(R - 1, C, N - 1)."
+    )
+    constraints.append(
+        "col_start_count(R, C, N) :- grid(R, C), col_count_range(C, N), not col_start(R, C), col_start_count(R - 1, C, N)."
+    )
+    constraints.append("col_end_count(-1, C, 0) :- grid(_, C).")
+    constraints.append(
+        "col_end_count(R, C, N) :- col_endgrid(R, C), col_count_range(C, N), col_end(R, C), col_end_count(R - 1, C, N - 1)."
+    )
+    constraints.append(
+        "col_end_count(R, C, N) :- col_endgrid(R, C), col_count_range(C, N), not col_end(R, C), col_end_count(R - 1, C, N)."
+    )
+    constraints.append(":- col_start_count(R, C, N1), col_end_count(R, C, N2), N1 > N2 + 1.")
+    constraints.append(":- col_start_count(R, C, N1), col_end_count(R, C, N2), N1 < N2.")
+    constraints.append(":- grid(R, C), col_end(R, C), col_start(R, C).")
+
     return "\n".join(constraints)
 
 
@@ -97,19 +116,17 @@ def solve(E: Encoding) -> List:
 
     top_clues = {}
     for c in E.top:
-        top_clues[c] = [int(clue) if clue != "?" else "?" for clue in E.top[c].split()]
+        top_clues[c] = tuple(int(clue) if clue != "?" else "?" for clue in E.top[c].split())
 
     left_clues = {}
     for r in E.left:
-        left_clues[r] = [int(clue) if clue != "?" else "?" for clue in E.left[r].split()]
+        left_clues[r] = tuple(int(clue) if clue != "?" else "?" for clue in E.left[r].split())
 
     solver.reset()
     solver.add_program_line(grid(E.R, E.C))
     solver.add_program_line(shade_c())
-
     solver.add_program_line(nono_row(E.R, E.C, left_clues))
     solver.add_program_line(nono_col(E.R, E.C, top_clues))
-
     solver.add_program_line(display())
     solver.solve()
 
