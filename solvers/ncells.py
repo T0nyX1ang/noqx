2c5a007ba0ba6d63d8108340906d4c1035614998
zhuyaoyu <1901112058@pku.edu.cn>
2024-02-15 23:57:47 +0800

    ncell solver in clingo

--- solvers/ncells.py
+++ solvers/ncells.py
@@ -1,37 +1,94 @@
-"""The N Cells solver."""
+"""The N Cell solver."""
 
 from typing import List
 
-from . import utils
-from .utils.claspy import set_max_val
-from .utils.borders import RectangularGridBorderSolver
-from .utils.encoding import Encoding
-from .utils.regions import RectangularGridRegionSolver
+from . import utilsx
+from .utilsx.encoding import Encoding
+from .utilsx.fact import display, grid
+from .utilsx.rule import (
+    adjacent,
+    connected,
+    connected_parts,
+    count_connected_parts,
+    rev_op_dict,
+)
+from .utilsx.helper import tag_encode
+from .utilsx.solution import solver
 
 
 def encode(string: str) -> Encoding:
-    return utils.encode(string, has_params=True)
+    return utilsx.encode(string, has_borders=True)
+
+
+def fill_num(low: int, high: int) -> str:
+    """
+    Generate a rule that a cell numbered between low and high.
+    """
+    num = f"num({low}..{high}).\n"
+    num += f"1 {{ number(R, C, N): num(N) }} 1 :- grid(R, C)."
+    return num
+
+
+def order_region() -> str:
+    """
+    Make sure region id is ordered, to avoid duplicate.
+    """
+    constriant = ":- num(N1), num(N2), N1<N2, #min{ (R, C): number(R, C, N1) } = (R1, C1), #min{ (R, C): number(R, C, N2) } = (R2, C2), (R1, C1) >= (R2, C2)."
+    return constriant
+
+
+def count_number(target: int, op: str = "eq") -> str:
+    """
+    Generates a constraint for counting the number in a grid.
+    A grid fact should be defined first.
+    """
+    op = rev_op_dict[op]
+    return f":- num(N), #count {{ R, C: number(R, C, N) }} {op} {target}."
+
+
+def connected_number(adj_type: int = 4) -> str:
+    """
+    Generate a rule to get all the connected components of {color} cells.
+    """
+    adj = tag_encode("adj", adj_type)
+    constraint = "reachable(R, C, R1, C1) :- grid(R, C), grid(R1, C1), R = R1, C = C1.\n"
+    constraint += f"reachable(R, C, R1, C1) :- grid(R, C), reachable(R, C, R2, C2), {adj}(R2, C2, R1, C1), num(N), number(R, C, N), number(R2, C2, N), number(R1, C1, N).\n"
+    constraint += ":- num(N), number(R, C, N), number(R1, C1, N), not reachable(R, C, R1, C1)."
+    return constraint
+
+
+def num_same_adj(r: int, c: int, number: int, adj_type: int = 4) -> str:
+    adj = tag_encode("adj", adj_type)
+    return f":- #count{{ R, C: {adj}({r}, {c}, R, C), num(N), number({r}, {c}, N), number(R, C, N) }} != {number}."
+
+
+def display_region() -> str:
+    """Generates a rule to show region ids."""
+    return "#show number/3."
 
 
 def solve(E: Encoding) -> List:
-    region_size = int(E.params["region_size"])
+    size = int(E.params["region_size"])
+    assert E.R * E.C % size == 0, "It's impossible to divide grid into regions of this size!"
+    region_num = E.R * E.C // size
 
-    assert (E.R * E.C) % region_size == 0, "It's impossible to divide grid into regions of this size!"
+    solver.reset(mode="region", R=E.R, C=E.C)
+    solver.add_program_line(grid(E.R, E.C))
+    solver.add_program_line(fill_num(0, region_num - 1))
+    solver.add_program_line(order_region())
+    solver.add_program_line(adjacent())
+    solver.add_program_line(count_number(region_num))
+    solver.add_program_line(connected_number())
 
-    # set the maximum IntVar value to the number of cells
-    set_max_val(E.R * E.C)
+    for (r, c), clue in E.clues.items():
+        solver.add_program_line(num_same_adj(r, c, 4-clue))
+        
+    solver.add_program_line(display_region())
+    # print(solver.program)
+    solver.solve()
 
-    region_solver = RectangularGridRegionSolver(E.R, E.C, max_num_regions=E.R * E.C)
-    border_solver = RectangularGridBorderSolver(E.R, E.C, region_solver)
-
-    region_solver.set_region_size(region_size, [], min_region_size=region_size)
-    region_solver.region_roots({})
-
-    for r, c in E.clues:
-        region_solver.set_num_neighbors_in_different_region(r, c, E.clues[(r, c)])
-
-    return border_solver.solutions()
+    return solver.solutions
 
 
 def decode(solutions: List[Encoding]) -> str:
-    return utils.decode(solutions)
+    return utilsx.decode(solutions)
--- solvers/utilsx/solution.py
+++ solvers/utilsx/solution.py
@@ -1,16 +1,32 @@
 """Generate solutions for the given problem."""
 
 from typing import Dict, List
+from enum import Enum
 
 from clingo.control import Control
 from clingo.solving import Model
 
 MAX_SOLUTIONS_TO_FIND = 10
+Direction = Enum("Direction", "LEFT TOP RIGHT BOTTOM")
 
 
 def rc_to_grid(r: int, c: int):
     """Convert row and column to compatible grid coordinates."""
-    return f"{2*r+1},{2*c+1}"
+    return f"{r*2+1},{c*2+1}"
+
+
+def rcd_to_edge(r, c, d):
+    """
+    Given an edge id, returns the border coordinate of the edge.
+    """
+    if d == Direction.TOP:
+        return f"{r*2},{c*2+1}"
+    elif d == Direction.LEFT:
+        return f"{r*2+1},{c*2}"
+    elif d == Direction.BOTTOM:
+        return f"{r*2+2},{c*2+1}"
+    elif d == Direction.RIGHT:
+        return f"{r*2+1},{c*2+2}"
 
 
 class ClingoSolver:
@@ -21,16 +37,43 @@ class ClingoSolver:
         self.clingo_instance: Control = Control()
         self.program: str = ""
         self.solutions: List[Dict[str, str]] = []
-
+        self.mode = "shade"
+        self.R, self.C = None, None
+    
     def store_solutions(self, model: Model):
         """Get the solution."""
         solution = tuple(str(model).split())  # raw solution converted from clingo
         formatted: Dict[str, str] = {}
+        if self.mode == "region":
+            assert self.R is not None and self.C is not None
+            region = [[None for _ in range(self.C)] for _ in range(self.R)]
 
         for item in solution:
-            color, coords = item.replace("(", " ").replace(")", " ").split()
-            r, c = coords.split(",")
-            formatted[rc_to_grid(int(r), int(c))] = color.replace("color", "")
+            if self.mode == "shade":
+                color, coords = item.replace("(", " ").replace(")", " ").split()
+                r, c = coords.split(",")
+                formatted[rc_to_grid(int(r), int(c))] = color.replace("color", "")
+            elif self.mode == "region":
+                _, coords = item.replace("(", " ").replace(")", " ").split()
+                r, c, id = map(int, coords.split(","))
+                region[r][c] = id
+            elif self.mode == "number":
+                raise NotImplementedError("Number mode not implemented!")
+        
+        if self.mode == "region":
+            print(region)
+            for r in range(self.R):
+                for c in range(self.C):
+                    if r and region[r][c] != region[r-1][c]:
+                        formatted[rcd_to_edge(r,c,Direction.TOP)] = "black"
+                    if c and region[r][c] != region[r][c-1]:
+                        formatted[rcd_to_edge(r,c,Direction.LEFT)] = "black"
+            for c in range(self.C):
+                formatted[rcd_to_edge(0,c,Direction.TOP)] = "black"
+                formatted[rcd_to_edge(self.R-1,c,Direction.BOTTOM)] = "black"
+            for r in range(self.R):
+                formatted[rcd_to_edge(r,0,Direction.LEFT)] = "black"
+                formatted[rcd_to_edge(r,self.C-1,Direction.RIGHT)] = "black"
 
         self.solutions.append(formatted)
 
@@ -38,8 +81,11 @@ class ClingoSolver:
         """Add a line to the program."""
         self.program += line + "\n"
 
-    def reset(self):
+    def reset(self, mode: str="color", R: int=None, C: int=None):
         """Reset the program."""
+        assert mode in ["shade", "region", "number"], "Mode must be in shade, region, number!"
+        self.mode = mode
+        self.R, self.C = R, C
         self.clingo_instance = Control()
         self.program = ""
         self.solutions = []
