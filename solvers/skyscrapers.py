bcb4851f376d96e539a25b79d30ba343c0d1dcaf
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2024-02-17 02:35:26 +0800

    perf: optimize skyscraper solver for larger cases

--- solvers/skyscrapers.py
+++ solvers/skyscrapers.py
@@ -9,15 +9,6 @@ from .utilsx.rule import fill_num, unique_num
 from .utilsx.solution import solver
 
 
-def skyscraper_blocked_rule() -> str:
-    """Generate rule for skyscraper puzzle."""
-    blocked = "blocked_t(R, C) :- number(R, C, N), number(R1, C, N1), R1 < R, N1 > N.\n"
-    blocked += "blocked_b(R, C) :- number(R, C, N), number(R1, C, N1), R1 > R, N1 > N.\n"
-    blocked += "blocked_l(R, C) :- number(R, C, N), number(R, C1, N1), C1 < C, N1 > N.\n"
-    blocked += "blocked_r(R, C) :- number(R, C, N), number(R, C1, N1), C1 > C, N1 > N."
-    return blocked
-
-
 def encode(string: str) -> Encoding:
     return utilsx.encode(string)
 
@@ -32,18 +23,21 @@ def solve(E: Encoding) -> List:
     solver.add_program_line(fill_num(_range=f"1..{n}"))
     solver.add_program_line(unique_num(_type="row", color="grid"))
     solver.add_program_line(unique_num(_type="col", color="grid"))
-    solver.add_program_line(skyscraper_blocked_rule())
 
     for c, num in E.top.items():
+        solver.add_program_line(f"blocked_t(R, {c}) :- number(R, {c}, N), number(R1, {c}, N1), R1 < R, N1 > N.")
         solver.add_program_line(f":- #count {{ R: blocked_t(R, {c}) }} != {n - num}.")
 
     for c, num in E.bottom.items():
+        solver.add_program_line(f"blocked_b(R, {c}) :- number(R, {c}, N), number(R1, {c}, N1), R1 > R, N1 > N.")
         solver.add_program_line(f":- #count {{ R: blocked_b(R, {c}) }} != {n - num}.")
 
     for r, num in E.left.items():
+        solver.add_program_line(f"blocked_l({r}, C) :- number({r}, C, N), number({r}, C1, N1), C1 < C, N1 > N.")
         solver.add_program_line(f":- #count {{ C: blocked_l({r}, C) }} != {n - num}.")
 
     for r, num in E.right.items():
+        solver.add_program_line(f"blocked_r({r}, C) :- number({r}, C, N), number({r}, C1, N1), C1 > C, N1 > N.")
         solver.add_program_line(f":- #count {{ C: blocked_r({r}, C) }} != {n - num}.")
 
     for (r, c), num in E.clues.items():
