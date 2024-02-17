2c11fcbbcfb28ffca0414698a997ad711f1cac7d
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2024-02-17 21:36:24 +0800

    feat: upgrade kakuro solver

--- solvers/kakuro.py
+++ solvers/kakuro.py
@@ -1,31 +1,30 @@
 """The Kakuro solver."""
 
-from typing import List
+from typing import List, Tuple
 
-from . import utils
-from .utils.claspy import IntVar, require, require_all_diff
-from .utils.encoding import Encoding
-from .utils.solutions import get_all_grid_solutions
+from . import utilsx
+from .utilsx.encoding import Encoding
+from .utilsx.fact import area, display, grid
+from .utilsx.rule import fill_num, unique_num
+from .utilsx.solution import solver
 
 
 def encode(string: str) -> Encoding:
-    return utils.encode(string)
+    return utilsx.encode(string)
 
 
 def solve(E: Encoding) -> List:
     # Find sections and their corresponding clues
-    # Maintain a list of tuples (sum, [(r1, c1), (r2, c2), ...]).
-    # 'sum' is 0 when the clue is blank; this is because we need to
-    # check every run for duplicates.
-    sums = []
+    # 'sum' is 0 when the clue is blank; this is because we need to check every run for duplicates.
+    sums: List[Tuple[int, List[Tuple[int, int]]]] = []
 
-    def end_run(start_coord, coords, idx):
+    def end_run(start_coord: Tuple[int, int], coords: List[Tuple[int, int]], idx: int):
         if start_coord in E.clues:
             value = E.clues[start_coord]
             if len(coords) > 0:
                 if value == "black":
                     sums.append((0, coords))
-                else:
+                elif isinstance(value, list):
                     sums.append((value[idx], coords))
             elif value != "black" and value[idx] != 0:
                 return False
@@ -46,6 +45,7 @@ def solve(E: Encoding) -> List:
             else:
                 coords.append((r, c))
         end_run(start_coord, coords, 0)
+
     # Down sections
     for c in range(E.C):
         start_coord = (-1, c)
@@ -60,22 +60,25 @@ def solve(E: Encoding) -> List:
                 coords.append((r, c))
         end_run(start_coord, coords, 1)
 
-    # Solve the puzzle!
-    grid = [[IntVar(0, 9) for c in range(E.C)] for r in range(E.R)]
+    area_id = 0
+
+    solver.reset()
+    solver.add_program_line(grid(E.R, E.C))
 
     for sum_clue, coord_list in sums:
-        intvars = [grid[r][c] for (r, c) in coord_list]
-        if sum_clue:
-            require(sum_clue == sum(intvars))
-        if len(intvars) > 1:  # require_all_diff throws an error if there's only 1 item
-            require_all_diff(intvars)
+        if len(coord_list) > 0:
+            solver.add_program_line(area(_id=area_id, src_cells=coord_list))
+            solver.add_program_line(fill_num(_range="1..9", _type="area", _id=area_id))
+            if sum_clue > 0:
+                solver.add_program_line(f":- #sum {{ N: area({area_id}, R, C), number(R, C, N) }} != {sum_clue}.")
+            area_id += 1
 
-    for r in range(E.R):
-        for c in range(E.C):
-            require((grid[r][c] == 0) == ((r, c) in E.clues))
+    solver.add_program_line(unique_num(_type="area", color="grid"))
+    solver.add_program_line(display(item="number", size=3))
+    solver.solve()
 
-    return get_all_grid_solutions(grid, format_function=lambda r, c: grid[r][c].value() if grid[r][c].value() else "")
+    return solver.solutions
 
 
 def decode(solutions: List[Encoding]) -> str:
-    return utils.decode(solutions)
+    return utilsx.decode(solutions)
