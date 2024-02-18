3e6608f652c95e42c59b3d736a2982bddb380ec6
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2024-02-19 00:38:37 +0800

    perf: slightly enhance the performance of fillomino

--- solvers/fillomino.py
+++ solvers/fillomino.py
@@ -5,7 +5,7 @@ from typing import List
 from . import utilsx
 from .utilsx.encoding import Encoding
 from .utilsx.fact import display, grid, edge
-from .utilsx.rule import adjacent, fill_num, reachable_edge
+from .utilsx.rule import adjacent, reachable_edge
 from .utilsx.solution import solver
 
 
@@ -15,13 +15,25 @@ def encode(string: str) -> Encoding:
 
 def fillomino_count():
     constriant = ":- number(R0, C0, N), #count { R, C: reachable_edge(R0, C0, R, C) } != N.\n"
+
+    # same number, adjacent cell, no line
     constriant += ":- number(R, C, N), number(R, C+1, N), vertical_line(R, C+1).\n"
     constriant += ":- number(R, C, N), number(R+1, C, N), horizontal_line(R+1, C).\n"
+
+    # different number, adjacent cell, have line
+    constriant += ":- number(R, C, N1), number(R, C+1, N2), N1 != N2, not vertical_line(R, C+1).\n"
+    constriant += ":- number(R, C, N1), number(R+1, C, N2), N1 != N2, not horizontal_line(R+1, C).\n"
+
+    # propagation of number
     constriant += "number(R, C, N) :- number(R0, C0, N), adj_edge(R0, C0, R, C).\n"
+
+    # special case for 1
     constriant += "{ horizontal_line(R, C); horizontal_line(R+1, C); vertical_line(R, C); vertical_line(R, C+1) } = 4 :- number(R, C, 1).\n"
     constriant += "number(R, C, 1) :- horizontal_line(R, C), horizontal_line(R+1, C), vertical_line(R, C), vertical_line(R, C+1).\n"
-    # constriant += ":- adj_4(R0, C0, R, C), not adj_edge(R0, C0, R, C), number(R0, C0, N), not number(R, C, _), #count{ R1, C1: reachable_edge(R, C, R1, C1) } = N.\n"
-    return constriant[:-1]
+
+    # case for unclued parts (unsafe)
+    constriant += ":- adj_4(R0, C0, R, C), not adj_edge(R0, C0, R, C), number(R, C, N), #count{ R1, C1: reachable_edge(R0, C0, R1, C1) } = N.\n"
+    return constriant.strip()
 
 
 def solve(E: Encoding) -> List:
@@ -43,11 +55,12 @@ def solve(E: Encoding) -> List:
     for (r, c), num in E.clues.items():
         solver.add_program_line(f"number({r}, {c}, {num}).")
 
-    # solver.add_program_line(fill_num(_range=f"1..{E.R * E.C}"))
     solver.add_program_line(display(item="vertical_line", size=2))
     solver.add_program_line(display(item="horizontal_line", size=2))
     solver.add_program_line(display(item="number", size=3))
     solver.solve()
+
+    print(solver.program)
 
     return solver.solutions
 
