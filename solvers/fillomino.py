709adba9c7bc26e634521a2b4392f32910162dff
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2024-02-21 04:59:54 +0800

    perf: upgrade fillomino solver even faster
    - add special judge for 1
    - make slow mode faster too

--- solvers/fillomino.py
+++ solvers/fillomino.py
@@ -13,7 +13,9 @@ def encode(string: str) -> Encoding:
     return utilsx.encode(string)
 
 
-def fillomino_count():
+def fillomino_constraint():
+    """Generate the Fillomino constraints."""
+
     # propagation of number
     constraint = "number(R, C, N) :- number(R0, C0, N), reachable_edge(R0, C0, R, C).\n"
     constraint += ":- number(R0, C0, N), #count { R, C: reachable_edge(R0, C0, R, C) } != N.\n"
@@ -27,23 +29,42 @@ def fillomino_count():
     constraint += ":- number(R, C, N1), number(R + 1, C, N2), N1 != N2, not horizontal_line(R + 1, C).\n"
 
     # special case for 1
-    constraint += "{ horizontal_line(R, C); horizontal_line(R + 1, C); vertical_line(R, C); vertical_line(R, C + 1) } = 4 :- number(R, C, 1).\n"
-    constraint += "number(R, C, 1) :- horizontal_line(R, C), horizontal_line(R + 1, C), vertical_line(R, C), vertical_line(R, C + 1).\n"
+    mutual = ["horizontal_line(R, C)", "horizontal_line(R + 1, C)", "vertical_line(R, C)", "vertical_line(R, C + 1)"]
+    constraint += f"{{ {'; '.join(mutual)} }} = 4 :- number(R, C, 1).\n"
+    constraint += f"number(R, C, 1) :- {', '.join(mutual)}.\n"
+    constraint += ":- number(R, C, 1), number(R1, C1, 1), adj_4(R, C, R1, C1).\n"
+
     return constraint.strip()
+
+
+def fillomino_slow() -> str:
+    """Generate the Fillomino rules for precise solving."""
+    count_edge = "#count{ R1, C1: reachable_edge(R, C, R1, C1) } = N"
+    slow = f":- adj_4(R, C, R0, C0), not reachable_edge(R, C, R0, C0), number(R0, C0, N), {count_edge}.\n"
+    slow += f"{{ numberx(R, C, N) }} = 1 :- grid(R, C), not number(R, C, _), {count_edge}.\n"
+    slow += ":- numberx(R, C, N), numberx(R1, C1, N), not reachable_edge(R, C, R1, C1), adj_4(R, C, R1, C1).\n"
+    return slow.strip()
 
 
 def solve(E: Encoding) -> List:
     solver.reset()
     solver.add_program_line(grid(E.R, E.C))
     solver.add_program_line(edge(E.R, E.C))
+    solver.add_program_line(adjacent(4))
     solver.add_program_line(adjacent("edge"))
     solver.add_program_line(reachable_edge())
-    solver.add_program_line(fillomino_count())
+    solver.add_program_line(fillomino_constraint())
 
     occurances = {1, 2, 3, 4, 5}
     for (r, c), num in E.clues.items():
         occurances.add(num)
         solver.add_program_line(f"number({r}, {c}, {num}).")
+
+        if num == 1:
+            solver.add_program_line(f"vertical_line({r}, {c}).")
+            solver.add_program_line(f"horizontal_line({r}, {c}).")
+            solver.add_program_line(f"vertical_line({r}, {c + 1}).")
+            solver.add_program_line(f"horizontal_line({r + 1}, {c}).")
 
     occurances = sorted(occurances)
     i, occ_seq = 0, []
@@ -66,16 +87,7 @@ def solve(E: Encoding) -> List:
     if E.params["fast"]:
         solver.add_program_line(fill_num(_range=f"{';'.join(occ_seq)}"))
     else:
-        solver.add_program_line(adjacent(4))
-        solver.add_program_line(
-            ":- adj_4(R0, C0, R, C), not adj_edge(R0, C0, R, C), number(R, C, N), #count{ R1, C1: reachable_edge(R0, C0, R1, C1) } = N."
-        )
-        solver.add_program_line(
-            "{ numberx(R, C, N) } = 1 :- grid(R, C), not number(R, C, _), #count{ R1, C1: reachable_edge(R, C, R1, C1) } = N."
-        )
-        solver.add_program_line(
-            ":- numberx(R, C, N), numberx(R1, C1, N), not adj_edge(R, C, R1, C1), adj_4(R, C, R1, C1)."
-        )
+        solver.add_program_line(fillomino_slow())
         solver.add_program_line(display(item="numberx", size=3))
 
     solver.solve()
