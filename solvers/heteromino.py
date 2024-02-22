67d3ea8451fbafc525e53f60c8db94a5430a4305
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2024-02-22 18:56:15 +0800

    refactor: generalize the codes of heteromino into shapes module

--- solvers/heteromino.py
+++ solvers/heteromino.py
@@ -7,38 +7,17 @@ from .utilsx.encoding import Encoding
 from .utilsx.fact import display, edge, grid
 from .utilsx.helper import tag_encode
 from .utilsx.rule import adjacent, split_by_edge
+from .utilsx.shape import all_same_shape, shape_omino
 from .utilsx.solution import solver
 
 
-def valid_omino_3(color: str = "black") -> str:
-    delta = (("-1", "+0"), ("+0", "-1"), ("+1", "+0"), ("+0", "+1"))
-    idx = 0
-    constraint = ""
-    t_va = tag_encode("valid_omino", 3, color)
-    t_be = tag_encode("belong_to_omino", 3, color)
-    for i in range(4):
-        dx1, dy1 = delta[i]
-        for j in range(i + 1, 4):
-            dx2, dy2 = delta[j]
-            idx += 1
-            remain = [delta[k] for k in range(4) if k not in [i, j]]
-            (dx3, dy3), (dx4, dy4) = remain
-            adj = f"adj_edge(R, C, R{dx1}, C{dy1}), adj_edge(R, C, R{dx2}, C{dy2}), not adj_edge(R, C, R{dx3}, C{dy3}), not adj_edge(R, C, R{dx4}, C{dy4})"
-            nadj = f"#count {{ R1, C1: adj_edge(R{dx1}, C{dy1}, R1, C1) }} = 1, #count {{ R1, C1: adj_edge(R{dx2}, C{dy2}, R1, C1) }} = 1"
-            constraint += f"{t_va}(R, C, {idx}) :- grid(R, C), {color}(R, C), {adj}, {nadj}.\n"
-    constraint += f"{t_be}(R, C, T) :- grid(R, C), {t_va}(R, C, T).\n"
-    constraint += f"{t_be}(R, C, T) :- grid(R, C), adj_edge(R, C, R1, C1), {t_va}(R1, C1, T).\n"
-    constraint += f":- grid(R, C), {color}(R, C), {{ {t_be}(R, C, T) }} < 1."
-    return constraint
-
-
-def avoid_adj_omino_3(color: str = "black") -> str:
+def avoid_adj_same_omino(color: str = "black") -> str:
     """
     Generates a constraint to avoid adjacent ominos with the same type.
 
     An split by edge rule, an omino rule should be defined first.
     """
-    t_be = tag_encode("belong_to_omino", 3, color)
+    t_be = tag_encode("belong_to_shape", "omino", 3, color)
     return f":- grid(R, C), grid(R1, C1), {t_be}(R, C, T), {t_be}(R1, C1, T), split_by_edge(R, C, R1, C1)."
 
 
@@ -67,8 +46,9 @@ def solve(E: Encoding) -> List:
         solver.add_program_line(f"horizontal_line({r}, {c}).")
         solver.add_program_line(f"horizontal_line({r + 1}, {c}).")
 
-    solver.add_program_line(valid_omino_3(color="not black"))
-    solver.add_program_line(avoid_adj_omino_3(color="not black"))
+    solver.add_program_line(shape_omino(3, color="not black", adj_type="edge", distinct_variant=True))
+    solver.add_program_line(all_same_shape("omino_3", color="not black"))
+    solver.add_program_line(avoid_adj_same_omino(color="not black"))
     solver.add_program_line(display(item="vertical_line", size=2))
     solver.add_program_line(display(item="horizontal_line", size=2))
     solver.solve()
--- solvers/utilsx/shape.py
+++ solvers/utilsx/shape.py
@@ -1,8 +1,60 @@
 """Rules and constraints to detect certain shapes."""
 
-from typing import Tuple
+from typing import Tuple, Iterable, List
 
-from .helper import tag_encode
+from .helper import tag_encode, get_variants
+
+
+OMINOES = {
+    1: {
+        ".": ((0, 0),),
+    },
+    2: {
+        "I": ((0, 0), (1, 0)),
+    },
+    3: {
+        "I": ((0, 0), (1, 0), (2, 0)),
+        "L": ((0, 0), (0, 1), (1, 0)),
+    },
+    4: {
+        "T": ((0, 0), (1, 0), (1, 1), (2, 0)),
+        "O": ((0, 0), (0, 1), (1, 0), (1, 1)),
+        "I": ((0, 0), (1, 0), (2, 0), (3, 0)),
+        "L": ((0, 0), (1, 0), (2, 0), (2, 1)),
+        "S": ((0, 0), (0, 1), (1, 1), (1, 2)),
+    },
+    5: {
+        "F": ((0, 0), (0, 1), (1, -1), (1, 0), (2, 0)),
+        "I": ((0, 0), (1, 0), (2, 0), (3, 0), (4, 0)),
+        "L": ((0, 0), (1, 0), (2, 0), (3, 0), (3, 1)),
+        "N": ((0, 0), (0, 1), (1, 1), (1, 2), (1, 3)),
+        "P": ((0, 0), (0, 1), (1, 0), (1, 1), (2, 0)),
+        "T": ((0, 0), (0, 1), (0, 2), (1, 1), (2, 1)),
+        "U": ((0, 0), (0, 2), (1, 0), (1, 1), (1, 2)),
+        "V": ((0, 0), (1, 0), (2, 0), (2, 1), (2, 2)),
+        "W": ((0, 0), (0, 1), (1, 1), (1, 2), (2, 2)),
+        "X": ((0, 0), (1, -1), (1, 0), (1, 1), (2, 0)),
+        "Y": ((0, 0), (1, -1), (1, 0), (1, 1), (1, 2)),
+        "Z": ((0, 0), (0, 1), (1, 1), (2, 1), (2, 2)),
+    },
+}
+
+
+def get_neighbor(r: int, c: int, _type: int = 4) -> Tuple[Tuple[int, int]]:
+    """Get the neighbors of a cell."""
+    shape_4 = ((r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1))
+    shape_x = ((r - 1, c - 1), (r - 1, c + 1), (r + 1, c - 1), (r + 1, c + 1))
+
+    if _type == 4:
+        return shape_4
+
+    if _type == "x":
+        return shape_x
+
+    if _type == 8:
+        return shape_4 + shape_x
+
+    raise ValueError("Invalid type, must be one of 4, 8, 'x'.")
 
 
 def all_rect(color: str = "black") -> str:
@@ -79,3 +131,75 @@ def valid_omino(num: int = 4, color: str = "black", _type: str = "grid", distinc
         return constraint
 
     raise ValueError("Invalid type, must be one of 'grid', 'area', 'edge'.")
+
+
+def general_shape(
+    name: str, deltas: Iterable[Tuple[int, int]], color: str = "black", _id: int = None, adj_type: int = 4
+) -> str:
+    """
+    Generates a rule for general shapes (using bruteforce technique).
+
+    A grid rule and an adjacent rule should be defined first.
+    """
+    tag = tag_encode("shape", name, color)
+    tag_belong = tag_encode("belong_to_shape", name, color)
+    neighbor_type = adj_type if adj_type in [4, 8, "x"] else 4
+    param = f"R, C, {_id}" if _id is not None else "R, C"
+    param0 = f"R0, C0, {_id}" if _id is not None else "R0, C0"
+
+    valid = []
+    belongs_to = []
+    for dr, dc in deltas:
+        valid.append(f"grid(R + {dr}, C + {dc}), {color}(R + {dr}, C + {dc})")
+        belongs_to.append(
+            f"{tag_belong}({param}) :- R = R0 + {dr}, C = C0 + {dc}, grid(R0 + {dr}, C0 + {dc}), {tag}({param0})."
+        )
+
+        sum_adj = 0
+        for nr, nc in get_neighbor(dr, dc, _type=neighbor_type):
+            if (nr, nc) in deltas:
+                sum_adj += 1
+                if adj_type not in [4, 8, "x"] and (dr < nr or dc < nc):
+                    valid.append(f"adj_{adj_type}(R + {dr}, C + {dc}, R + {nr}, C + {nc})")
+        valid.append(f"#count {{ R1, C1: {color}(R1, C1), adj_{adj_type}(R + {dr}, C + {dc}, R1, C1) }} = {sum_adj}")
+
+    return f"{tag}({param}) :- {', '.join(valid)}.\n" + "\n".join(belongs_to)
+
+
+def all_same_shape(name: str, color: str = "black") -> str:
+    """
+    Generates a constraint to force all cells to be the same shape.
+
+    A grid rule and a belong_to_shape rule should be defined first.
+    """
+    tag = tag_encode("belong_to_shape", name, color)
+    return f":- grid(R, C), {color}(R, C), {{ {tag}(R, C, T) }} < 1."
+
+
+def shape_omino(
+    num: int,
+    omino_types: List[str] = None,
+    color: str = "black",
+    adj_type: int = 4,
+    distinct_variant: bool = False,
+):
+    """
+    Generates a rule for a valid omino.
+
+    A grid rule or an area rule should be defined first.
+    """
+
+    if omino_types is None:
+        omino_types = list(OMINOES[num].keys())
+
+    data, total = "", 0
+    for omino_type in omino_types:
+        omino_shape = OMINOES[num][omino_type]
+        omino_variants = get_variants(omino_shape, allow_rotations=True, allow_reflections=True)
+
+        for variant in omino_variants:
+            if distinct_variant:
+                total += 1
+            data += general_shape(f"omino_{num}", variant, color, _id=total, adj_type=adj_type) + "\n"
+
+    return data.strip()
