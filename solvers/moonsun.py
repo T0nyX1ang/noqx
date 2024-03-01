41cd4f7659f04de94ced3d7dfc4a8a45d35f7c3b
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2024-03-02 00:04:14 +0800

    chore: using a uniform API in moon-or-sun solver

--- solvers/moonsun.py
+++ solvers/moonsun.py
@@ -7,46 +7,56 @@ from .utilsx.encoding import Encoding
 from .utilsx.fact import area, direction, display, grid
 from .utilsx.loop import connected_loop, fill_path, pass_area_one_time, single_loop
 from .utilsx.region import full_bfs
-from .utilsx.rule import adjacent
+from .utilsx.rule import adjacent, area_adjacent, shade_c
 from .utilsx.solution import solver
 
 
-def encode(string: str) -> Encoding:
-    return utilsx.encode(string, has_borders=True)
-
-
 def moon_sun_area() -> str:
+    """
+    Genearate a constraint to determine the area of the moon or sun.
+    A sun area should only contain sun cells, and a moon area should only contain moon cells.
+    A sun area should be adjacent to a moon area, and vice versa.
+
+    A grid fact and an area adjacent rule should be defined first.
+    """
     rule = "{ sun_area(A) } :- area(A, _, _).\n"
     rule += ":- sun_area(A), area(A, R, C), sun(R, C), not moon_sun_loop(R, C).\n"
     rule += ":- sun_area(A), area(A, R, C), moon(R, C), moon_sun_loop(R, C).\n"
     rule += ":- not sun_area(A), area(A, R, C), sun(R, C), moon_sun_loop(R, C).\n"
     rule += ":- not sun_area(A), area(A, R, C), moon(R, C), not moon_sun_loop(R, C).\n"
 
-    adj_diff = "adj_area(A1, A2) :- area(A1, R1, C1), area(A2, R2, C2), A1 < A2, adj_loop(R1, C1, R2, C2).\n"
-    adj_diff += ":- adj_area(A1, A2), sun_area(A1), sun_area(A2).\n"
-    adj_diff += ":- adj_area(A1, A2), not sun_area(A1), not sun_area(A2).\n"
-    return (rule + adj_diff).strip()
+    constraint = ":- area_adj_loop(A1, A2), sun_area(A1), sun_area(A2).\n"
+    constraint += ":- area_adj_loop(A1, A2), not sun_area(A1), not sun_area(A2).\n"
+    return (rule + constraint).strip()
+
+
+def encode(string: str) -> Encoding:
+    return utilsx.encode(string, has_borders=True)
 
 
 def solve(E: Encoding) -> List:
     solver.reset()
     solver.add_program_line(grid(E.R, E.C))
     solver.add_program_line(direction("lurd"))
-    solver.add_program_line("{ moon_sun_loop(R, C) } :- grid(R, C).")
+    solver.add_program_line(shade_c(color="moon_sun_loop"))
     solver.add_program_line(fill_path(color="moon_sun_loop"))
     solver.add_program_line(adjacent(_type="loop"))
     solver.add_program_line(connected_loop(color="moon_sun_loop"))
     solver.add_program_line(single_loop(color="moon_sun_loop", visit_all=True))
 
     for (r, c), clue in E.clues.items():
-        _type = "sun" if clue == "s" else "moon"
-        solver.add_program_line(f"{_type}({r}, {c}).")
+        if clue == "m":
+            solver.add_program_line(f"moon({r}, {c}).")
+        elif clue == "s":
+            solver.add_program_line(f"sun({r}, {c}).")
 
     areas = full_bfs(E.R, E.C, E.edges)
+    assert len(areas) % 2 == 0, "The number of areas should be even."
     for i, ar in enumerate(areas):
         solver.add_program_line(area(_id=i, src_cells=ar))
         solver.add_program_line(pass_area_one_time(ar))
 
+    solver.add_program_line(area_adjacent(adj_type="loop"))
     solver.add_program_line(moon_sun_area())
     solver.add_program_line(display(item="loop_sign", size=3))
     solver.solve()
