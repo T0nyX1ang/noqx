e42eb32cbd7d3490df0c81b11935fcd4db3ef038
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2024-03-01 23:02:11 +0800

    refactor: using a uniform API (avoid_area_adjacent)

--- solvers/countryroad.py
+++ solvers/countryroad.py
@@ -21,26 +21,21 @@ def solve(E: Encoding) -> List:
     solver.add_program_line(direction("lurd"))
     solver.add_program_line(shade_c(color="country_road"))
     solver.add_program_line(fill_path(color="country_road"))
+    solver.add_program_line(adjacent(_type=4))
     solver.add_program_line(adjacent(_type="loop"))
     solver.add_program_line(connected_loop(color="country_road"))
     solver.add_program_line(single_loop(color="country_road", visit_all=True))
 
-    num_dict = {}
-    for (r, c), clue in E.clues.items():
-        num_dict[(r, c)] = clue
-
     areas = full_bfs(E.R, E.C, E.edges)
     for i, ar in enumerate(areas):
-        for r, c in ar:
-            if (r, c) in num_dict:
-                solver.add_program_line(area(_id=i, src_cells=ar))
-                solver.add_program_line(count(num_dict[(r, c)], color="country_road", _type="area", _id=i))
-            if (r + 1, c) not in ar and r + 1 < E.R:
-                solver.add_program_line(f":- not country_road({r}, {c}), not country_road({r+1}, {c}).")
-            if (r, c + 1) not in ar and c + 1 < E.C:
-                solver.add_program_line(f":- not country_road({r}, {c}), not country_road({r}, {c+1}).")
+        solver.add_program_line(area(_id=i, src_cells=ar))
         solver.add_program_line(pass_area_one_time(ar))
 
+        for rc in ar:
+            if rc in E.clues:
+                solver.add_program_line(count(E.clues[rc], color="country_road", _type="area", _id=i))
+
+    solver.add_program_line(avoid_area_adjacent(color="not country_road"))
     solver.add_program_line(display(item="loop_sign", size=3))
     solver.solve()
 
