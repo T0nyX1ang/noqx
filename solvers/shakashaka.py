f7528556e134a97fb270672ec30639f9193bbd76
zhuyaoyu <1901112058@pku.edu.cn>
2024-03-02 12:21:19 +0800

    fix: bug in shakashaka clingo solver

--- solvers/shakashaka.py
+++ solvers/shakashaka.py
@@ -17,16 +17,14 @@ def shaka(R: int, C: int) -> str:
 
     A grid rule should be defined first.
     """
-    definition = 'white_down(R, C) :- grid(R, C), white(R, C).\n'
+    definition = "white_down(R, C) :- grid(R, C), white(R, C).\n"
     definition += 'white_down(R, C) :- grid(R, C), triangle(R, C, "ul").\n'
     definition += 'white_down(R, C) :- grid(R, C), triangle(R, C, "ur").\n'
-    definition += 'white_right(R, C) :- grid(R, C), white(R, C).\n'
+    definition += "white_right(R, C) :- grid(R, C), white(R, C).\n"
     definition += 'white_right(R, C) :- grid(R, C), triangle(R, C, "ul").\n'
     definition += 'white_right(R, C) :- grid(R, C), triangle(R, C, "dl").\n'
 
-    rule_rect = 'rect_ul(R, C) :- grid(R, C), white(R, C), not white_down(R - 1, C), not white_right(R, C - 1).\n'
-    # rule_rect = 'not white_down(R - 1, C) :- grid(R, C), white(R, C), rect_ul(R, C).\n'
-    # rule_rect = 'not white_right(R, C - 1) :- grid(R, C), white(R, C), rect_ul(R, C).\n'
+    rule_rect = "rect_ul(R, C) :- grid(R, C), white(R, C), not white_down(R - 1, C), not white_right(R, C - 1).\n"
     rule_rect += "rect_l(R, C) :- grid(R, C), white(R, C), rect_ul(R - 1, C), not white_right(R, C - 1).\n"
     rule_rect += "rect_l(R, C) :- grid(R, C), white(R, C), rect_l(R - 1, C), not white_right(R, C - 1).\n"
     rule_rect += "rect_u(R, C) :- grid(R, C), white(R, C), rect_ul(R, C - 1), not white_down(R - 1, C).\n"
@@ -34,29 +32,28 @@ def shaka(R: int, C: int) -> str:
     rule_rect += "remain(R, C) :- grid(R, C), rect_l(R, C - 1), rect_u(R - 1, C).\n"
     rule_rect += "remain(R, C) :- grid(R, C), rect_l(R, C - 1), remain(R - 1, C).\n"
     rule_rect += "remain(R, C) :- grid(R, C), remain(R, C - 1), rect_u(R - 1, C).\n"
-    rule_rect += "remain(R, C) :- grid(R, C), remain(R, C - 1), remain(R - 1, C).\n"
+    rule_rect += 'remain(R, C) :- grid(R, C), remain(R, C - 1), remain(R - 1, C), not triangle(R, C, "dr").\n'
 
     rule_slant = 'slant_ul(R, C) :- grid(R, C), triangle(R, C, "ul"), triangle(R, C+1, "ur"), triangle(R+1, C, "dl").\n'
     rule_slant += 'slant_ul(R, C) :- grid(R, C), triangle(R, C, "ul"), triangle(R, C+1, "ur"), triangle(R+1, C-1, "ul").\n'
     rule_slant += 'slant_ul(R, C) :- grid(R, C), triangle(R, C, "ul"), triangle(R+1, C, "dl"), triangle(R-1, C+1, "ul").\n'
-    rule_slant += 'slant_ul(R, C) :- grid(R, C), triangle(R, C, "ul"), triangle(R-1, C+1, "ul"), triangle(R+1, C-1, "ul").\n'
+    rule_slant += 'slant_ul(R, C) :- grid(R, C), triangle(R, C, "ul"), triangle(R-1, C+1, "ul"), triangle(R+1, C-1, "ul"), white(R+1, C), white(R, C+1).\n'
     rule_slant += 'slant_ur(R, C) :- grid(R, C), triangle(R, C, "ur"), triangle(R, C-1, "ul"), triangle(R+1, C, "dr").\n'
     rule_slant += 'slant_ur(R, C) :- grid(R, C), triangle(R, C, "ur"), triangle(R, C-1, "ul"), triangle(R+1, C+1, "ur").\n'
     rule_slant += 'slant_ur(R, C) :- grid(R, C), triangle(R, C, "ur"), triangle(R+1, C, "dr"), triangle(R-1, C-1, "ur").\n'
-    rule_slant += 'slant_ur(R, C) :- grid(R, C), triangle(R, C, "ur"), triangle(R-1, C-1, "ur"), triangle(R+1, C+1, "ur").\n'
+    rule_slant += 'slant_ur(R, C) :- grid(R, C), triangle(R, C, "ur"), triangle(R-1, C-1, "ur"), triangle(R+1, C+1, "ur"), white(R+1, C), white(R, C-1).\n'
     rule_slant += 'slant_dl(R, C) :- grid(R, C), triangle(R, C, "dl"), triangle(R, C+1, "dr"), triangle(R-1, C, "ul").\n'
     rule_slant += 'slant_dl(R, C) :- grid(R, C), triangle(R, C, "dl"), triangle(R, C+1, "dr"), triangle(R-1, C-1, "dl").\n'
     rule_slant += 'slant_dl(R, C) :- grid(R, C), triangle(R, C, "dl"), triangle(R-1, C, "ul"), triangle(R+1, C+1, "dl").\n'
-    rule_slant += 'slant_dl(R, C) :- grid(R, C), triangle(R, C, "dl"), triangle(R-1, C-1, "dl"), triangle(R+1, C+1, "dl").\n'
+    rule_slant += 'slant_dl(R, C) :- grid(R, C), triangle(R, C, "dl"), triangle(R-1, C-1, "dl"), triangle(R+1, C+1, "dl"), white(R-1, C), white(R, C+1).\n'
     rule_slant += 'slant_dr(R, C) :- grid(R, C), triangle(R, C, "dr"), triangle(R, C-1, "dl"), triangle(R-1, C, "ur").\n'
     rule_slant += 'slant_dr(R, C) :- grid(R, C), triangle(R, C, "dr"), triangle(R, C-1, "dl"), triangle(R-1, C+1, "dr").\n'
     rule_slant += 'slant_dr(R, C) :- grid(R, C), triangle(R, C, "dr"), triangle(R-1, C, "ur"), triangle(R+1, C-1, "dr").\n'
     rule_slant += 'slant_dr(R, C) :- grid(R, C), triangle(R, C, "dr"), triangle(R-1, C+1, "dr"), triangle(R+1, C-1, "dr"), white(R-1, C), white(R, C-1).\n'
-    rule_slant += 'remain(R, C) :- grid(R, C), slant_ul(R, C-1), slant_ul(R-1, C).\n'
-    rule_slant += 'remain(R, C) :- grid(R, C), slant_dl(R, C-1), slant_dl(R+1, C).\n'
-    rule_slant += 'remain(R, C) :- grid(R, C), slant_ur(R, C+1), slant_ur(R-1, C).\n'
-
-    # constraint = f"black(R, C) :- -1 <= R, R <= {R}, -1 <= C, C <= {C}, not grid(R, C).\n"
+    rule_slant += "remain(R, C) :- grid(R, C), slant_ul(R, C-1), slant_ul(R-1, C).\n"
+    rule_slant += "remain(R, C) :- grid(R, C), slant_dl(R, C-1), slant_dl(R+1, C).\n"
+    rule_slant += "remain(R, C) :- grid(R, C), slant_ur(R, C+1), slant_ur(R-1, C).\n"
+    # rule_slant += 'remain(R, C) :- grid(R, C), slant_dr(R, C+1), slant_dr(R+1, C).\n'
 
     constraint = f':- grid(R, C), triangle(R, C, "ul"), not slant_ul(R, C).\n'
     constraint += f':- grid(R, C), triangle(R, C, "ur"), not slant_ur(R, C).\n'
@@ -92,9 +89,7 @@ def solve(E: Encoding) -> List:
         solver.add_program_line(f"black({r}, {c}).")
 
     solver.add_program_line(shaka(E.R, E.C))
-    solver.add_program_line(display(item="black"))
     solver.add_program_line(display(item="triangle", size=3))
-    # print(solver.program)
     solver.solve()
 
     return solver.solutions
@@ -102,4 +97,3 @@ def solve(E: Encoding) -> List:
 
 def decode(solutions: List[Encoding]) -> str:
     return utilsx.decode(solutions)
-
