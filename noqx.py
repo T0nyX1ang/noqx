8a4212ed835e57df3d22b10ac7903b0a034fcb4c
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2024-04-17 23:22:36 +0800

    feat: support script loading

--- noqx.py
+++ noqx.py
@@ -4,6 +4,7 @@ Main file for the noqx project.
 execute "uvicorn noqx:app" to start the server.
 """
 
+import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
@@ -11,7 +12,6 @@ from fastapi.templating import Jinja2Templates
 
 from solvers.utilsx import run
 from solvers.utilsx.consts import CATEGORIES, PUZZLE_TYPES
-
 
 app = FastAPI(title="noqx", description="An extended logic puzzle solver.")
 
@@ -22,7 +22,8 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 async def root_page(request: Request):
     """The root endpoint of the server."""
-    return templates.TemplateResponse(request=request, name="index.html", context={"types": PUZZLE_TYPES, "cats": CATEGORIES})
+    context = {"types": PUZZLE_TYPES, "cats": CATEGORIES}
+    return templates.TemplateResponse(request=request, name="index.html", context=context)
 
 
 @app.get("/{puzzle_type}", response_class=HTMLResponse)
@@ -30,9 +31,8 @@ async def puzzle_page(request: Request, puzzle_type: str):
     """The puzzle endpoint of the server."""
     for _pt_dict in PUZZLE_TYPES:
         if _pt_dict["value"] == puzzle_type:
-            return templates.TemplateResponse(
-                request=request, name="noq.html", context={"name": _pt_dict["name"], "value": _pt_dict["value"]}
-            )
+            context = {"name": _pt_dict["name"], "value": _pt_dict["value"]}
+            return templates.TemplateResponse(request=request, name="noq.html", context=context)
 
         if "aliases" in _pt_dict and puzzle_type in _pt_dict["aliases"]:
             return RedirectResponse(url=f"/{_pt_dict['value']}")
@@ -42,3 +42,7 @@ async def puzzle_page(request: Request, puzzle_type: str):
 def solver(puzzle_type: str, puzzle: str):  # clingo might be incompatible with asyncio
     """The solver endpoint of the server."""
     return run(puzzle_type, puzzle)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app="noqx:app", host="127.0.0.1", port=8000, reload=False)
