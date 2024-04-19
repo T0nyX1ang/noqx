b3e29e6f086c24269224fc27617afc8437e09ebd
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2024-04-19 16:50:42 +0800

    chore: add spec file for packaging purposes

--- fix-imports.py
+++
@@ -1,9 +0,0 @@
-"""This file fixes the hidden imports for pyinstaller."""
-
-import os
-
-print("import noqx")
-
-for files in os.listdir("solvers"):
-    if files.endswith(".py") and not files.startswith("__"):
-        print(f"import solvers.{files[:-3]}")
