e93aebcb1e4ffaf1e570a8ead6b8e916e12381ce
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2024-08-02 01:58:26 +0800

    chore: change variable names for penpa-edit

--- solver/core/penpa.py
+++ solver/core/penpa.py
@@ -52,12 +52,12 @@ class Puzzle:
         self.__parts = decompress(b64decode(self.content[len(PENPA_PREFIX) :]), -15).decode().split("\n")
 
         self.cell_shape: Optional[str] = None
-        self.width: int = 0
-        self.height: int = 0
-        self.top_space: int = 0
-        self.bottom_space: int = 0
-        self.left_space: int = 0
-        self.right_space: int = 0
+        self.cols: int = 0
+        self.rows: int = 0
+        self.top_rows: int = 0
+        self.bottom_rows: int = 0
+        self.left_cols: int = 0
+        self.right_cols: int = 0
         self._init_size()
 
         self._init_board()
@@ -72,14 +72,14 @@ class Puzzle:
 
         if header[0] in ("square", "sudoku", "kakuro"):
             self.cell_shape = "square"
-            self.top_space, self.bottom_space, self.left_space, self.right_space = json.loads(self.__parts[1])
-            self.width = int(header[1]) - self.left_space - self.right_space
-            self.height = int(header[2]) - self.top_space - self.bottom_space
+            self.top_rows, self.bottom_rows, self.left_cols, self.right_cols = json.loads(self.__parts[1])
+            self.rows = int(header[2]) - self.top_rows - self.bottom_rows
+            self.cols = int(header[1]) - self.left_cols - self.right_cols
         else:
             raise ValueError("Unsupported cell shape. Current only supported square shape.")
 
-        margin = (self.top_space, self.bottom_space, self.left_space, self.right_space)
-        print(f"Puzzle size initialized. Size: {self.width}x{self.height}. Margin: {margin}.")
+        margin = (self.top_rows, self.bottom_rows, self.left_cols, self.right_cols)
+        print(f"Puzzle size initialized. Size: {self.rows}x{self.cols}. Margin: {margin}.")
 
     def _init_board(self):
         """Initialize the content of the puzzle."""
@@ -89,6 +89,7 @@ class Puzzle:
         for index, _ in board["surface"].items():
             coord, _ = self.index_to_coord(int(index))
             self.surface[coord] = "gray"  # fix color to gray
+        print(self.surface)
 
         self.number = {}
         for index, num_data in board["number"].items():
@@ -98,18 +99,15 @@ class Puzzle:
                 self.number[coord] = list(map(int, list(num_data[0])))
             elif num_data[2] != "7":  # neglect candidates
                 self.number[coord] = int(num_data[0])
-
             # TODO: handle non-number texts
-
-        print(board)
+        print(self.number)
 
     def index_to_coord(self, index: int) -> Tuple[Tuple[int, int], int]:
         """Convert the penpa index to coordinate."""
-        real_width = self.width + self.left_space + self.right_space + 4
-        real_height = self.height + self.top_space + self.bottom_space + 4
-        category = index // (real_width * real_height)
-        index = index % (real_width * real_height)
-        return (index // real_height - 2, index % real_width - 2), category
+        real_rows = self.rows + self.top_rows + self.bottom_rows + 4
+        real_cols = self.cols + self.left_cols + self.right_cols + 4
+        category, index = divmod(index, real_rows * real_cols)
+        return (index // real_rows - 2, index % real_cols - 2), category
 
     def coord_to_index(self):
         """Convert the coordinate to penpa index."""
