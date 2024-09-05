11774adf8bb883beaa1542263dc8f8c7ca28e419
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2024-09-05 16:51:43 +0800

    fix: issue in translation with missing ids

---
+++ static/penpa_const.js
@@ -0,0 +1,19 @@
+const Identity = {
+  appOwner: "Noqx",
+  okButtonText: "ok 🙂",
+  errorTitle: "Noqx says",
+  infoTitle: "Noqx says",
+  solveTitle: undefined,
+  solveDefaultMessage: "Congratulations 🙂", // Default - Congratulations 🙂
+  solveOKButtonText: "Hurray!",
+  incorrectMessage: "Keep trying 🙂",
+  addUsageButtons: {},
+};
+
+const style_tag_cache = {
+  color_theme: "link#color_theme",
+  base_structure: "link#base_structure",
+  tab_setting_css: "link#tab_setting_css",
+  custom_color_css: "link#custom_color_css",
+  constraints_settings_css: "link#constraints_settings_css",
+};
