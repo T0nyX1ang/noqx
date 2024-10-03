7f5a3aa7fdb526eebe60249d48f02ce1e503e239
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2024-10-03 19:49:52 +0800

    feat: initial implementation of the penalty theory

--- solver/heyawake.py
+++ solver/heyawake.py
@@ -1,5 +1,6 @@
 """The Heyawake solver."""
 
+import itertools
 from typing import Iterable, List, Tuple, Union
 
 from .core.common import area, count, display, grid, shade_c
@@ -28,6 +29,12 @@ def avoid_area_2x2_rect(_id: int, color: str = "black") -> str:
     return rule
 
 
+def avoid_area_isolated_inner_cell(_id: int, color: str = "black") -> str:
+    """Avoid isolated non-border cells in areas."""
+    rule = f":- area({_id}, R, C), area({_id}, R - 1, C - 1), area({_id}, R - 1, C + 1), area({_id}, R + 1, C - 1), area({_id}, R + 1, C + 1), {color}(R, C), not {color}(R - 1, C - 1), not {color}(R - 1, C + 1), not {color}(R + 1, C - 1), not {color}(R + 1, C + 1).\n"
+    return rule
+
+
 def area_border(_id: int, ar: Iterable[Tuple[int, int]]) -> str:
     """Generates a fact for the border of an area."""
     borders = set()
@@ -36,8 +43,59 @@ def area_border(_id: int, ar: Iterable[Tuple[int, int]]) -> str:
             r1, c1 = r + dr, c + dc
             if (r1, c1) not in ar:
                 borders.add((r, c))
+
     rule = "\n".join(f"area_border({_id}, {r}, {c})." for r, c in borders)
     return rule
+
+
+def calculate_penalty(_id: int, ar: Iterable[Tuple[int, int]], puzzle: Puzzle) -> int:
+    """Calculate penalties of an area."""
+    rows, cols = puzzle.row, puzzle.col
+
+    def calculate_each_border_penalty(r: int = 0, c: int = 0) -> int:
+        i, segment_border_penalty = 0, 0
+        while i < cols:
+            segment = 0
+            while (r, i) in ar and i < cols:
+                segment += 1
+                i += 1
+
+            segment_border_penalty += (segment + 1) // 2
+            i += 1
+
+        i = 0
+        while i < rows:
+            segment = 0
+            while (i, c) in ar and i < cols:
+                segment += 1
+                i += 1
+
+            segment_border_penalty += (segment + 1) // 2
+            i += 1
+
+        return segment_border_penalty
+
+    loop_penalty = 0
+    for r, c in itertools.product(range(rows), range(cols)):
+        plus_segment = 0
+        for dr, dc in ((0, 0), (0, 1), (1, 0), (1, 1)):
+            r1, c1 = r + dr, c + dc
+            if (r1, c1) not in ar and puzzle.surface.get((r1, c1)) in [1, 3, 4, 8]:
+                plus_segment = 0
+                break
+
+            plus_segment += (r1, c1) in ar
+
+        if plus_segment >= 2 and r + 1 < rows and c + 1 < cols:
+            loop_penalty += 1
+
+    border_penalty = 0
+    border_penalty += calculate_each_border_penalty(0, 0)
+    border_penalty += calculate_each_border_penalty(rows - 1, cols - 1)
+
+    print("Area", _id, "has total penalty of", loop_penalty + border_penalty)
+
+    return loop_penalty + border_penalty
 
 
 def area_border_connected(_id: int, color: str = "black", adj_type: Union[int, str] = 4) -> str:
@@ -70,6 +128,9 @@ def solve(puzzle: Puzzle) -> List[Solution]:
     areas = full_bfs(puzzle.row, puzzle.col, puzzle.edge, puzzle.text)
     for i, (ar, rc) in enumerate(areas.items()):
         solver.add_program_line(area(_id=i, src_cells=ar))
+        calculate_penalty(_id=i, ar=ar, puzzle=puzzle)
+
+        print("Area", i, "has total shaded of", puzzle.text[rc] if rc else "N/A")
 
         if rc:
             data = puzzle.text[rc]
@@ -78,6 +139,7 @@ def solve(puzzle: Puzzle) -> List[Solution]:
 
             if puzzle.param["fast_mode"] and data > len(ar) // 4:
                 solver.add_program_line(avoid_area_2x2_rect(_id=i, color="gray"))
+                solver.add_program_line(avoid_area_isolated_inner_cell(_id=i, color="gray"))
                 solver.add_program_line(area_border(_id=i, ar=ar))
                 solver.add_program_line(area_border_connected(_id=i, color="gray", adj_type="x"))
 
