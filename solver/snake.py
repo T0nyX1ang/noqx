67cb95256186e40e158cf3a9c96130502cfe0200
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2024-11-15 13:17:38 +0800

    fix: snake not detecting dead ends properly

--- solver/snake.py
+++ solver/snake.py
@@ -55,10 +55,10 @@ def solve(puzzle: Puzzle) -> List[Solution]:
     for (r, c), symbol_name in puzzle.symbol.items():
         if symbol_name == "circle_L__1__0":
             solver.add_program_line(f"gray({r}, {c}).")
-            solver.add_program_line(f"not dead_end({r}, {c}).")
+            solver.add_program_line(f":- dead_end({r}, {c}).")
         if symbol_name == "circle_L__2__0":
             solver.add_program_line(f"gray({r}, {c}).")
-            solver.add_program_line(f"dead_end({r}, {c}).")
+            solver.add_program_line(f":- not dead_end({r}, {c}).")
 
     for (r, c), color_code in puzzle.surface.items():
         if color_code in [1, 3, 4, 8]:  # shaded color (DG, GR, LG, BK)
@@ -80,5 +80,9 @@ __metadata__ = {
             "data": "m=edit&p=7VRRb5swEH7Pr6j87AfbQCC8ZV2zlyzdlkxVhKKIMKpEJSODME2O8t/73UFHO5C2aVqeKuPTd9/Z+ON8XPmtiotUapceJ5BKagzPVzxdf8hTNWOxO2ZpeCXH1XGbFwBS3k4m8j7OynQQ0U6M1eBkR6EdS/sujIQWUhhMLVbSfgxP9n1ol9LOERIyADetFxnAmxbecZzQdU1qBTxrMOASMNkVSZaupzXzIYzsQgo65w3vJij2+fdUNDrIT/L9ZkfEJj7iY8rt7tBEyupL/lA1a/XqLO24ljvvkeu0cgnWcgn1yKWv+He52SHvEzpanc9I+CdIXYcRqf7cwqCF8/AEOwtPwtXY6uGm+E6E68ANWteFa1p3CBfF8eQGcJ2frqdeuD7tpRqoXa0p3L5aDynevswoij/zHTrs6Wyo1ax5Cc2ez5GXKRQjkt5htR6xjF9pFttD00u6dEAndmijTC9t+lc7vUcat3+153VpfP+Es2DYLnCZ0jps37JVbD22U15zw/aO7TVbl+2Q1/hUDn9YMJz2APcKYaaunue38p+0RZ5fN5LOeOWbBhuJeVXcx0mKjjCr9pu0uJrlxT7O4M+38SEVaMLngfghePIv6r725Yv3ZUq++qvufIH/6zdyIuQVf6C9leJQreN1kqOqkDXifa/DX1w9GoQov8YPVOOP",
         },
         {"url": "https://puzz.link/p?snake/11/11/00000000000000000000000000000000000000000957664857598o9", "test": False},
+        {
+            "url": "https://puzz.link/p?snake/15/15/13a3b00000a3d3a00000a3a4a00030a3a3a10000d3aca03001a3a3a10039a3d3a00100j3a39zp",
+            "test": False,
+        },
     ],
 }
