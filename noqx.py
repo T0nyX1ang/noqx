ce3044ebf81bb19fec4a92452b6ea037189b99f6
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2024-12-02 02:50:45 +0800

    feat: change logging utility to loguru

--- noqx.py
+++ noqx.py
@@ -1,15 +1,19 @@
-"""Main file for the noqx project."""
+"""Entry point for the noqx project."""
 
 import argparse
+import sys
 
 import uvicorn
 from starlette.config import environ
 
+from noqx.logging import logger
 from noqx.manager import load_solvers
 from noqx.solution import Config
 
-load_solvers("solver")  # default solver directory
+# load default solver directory
+load_solvers("solver")
 
+# argument parser
 parser = argparse.ArgumentParser(description="noqx startup settings.")
 parser.add_argument("-H", "--host", default="127.0.0.1", type=str, help="the host to run the server on.")
 parser.add_argument("-p", "--port", default=8000, type=int, help="the port to run the server on.")
@@ -20,6 +24,30 @@ args = parser.parse_args()
 Config.time_limit = args.time_limit
 Config.parallel_threads = args.parallel_threads
 
+# logging setup
+log_level = "DEBUG" if args.debug else "INFO"
+log_format = "<g>{time:MM-DD HH:mm:ss}</g> | <lvl>{level}</lvl> | {message}"
+logger.add(sys.stdout, level=log_level, diagnose=False, format=log_format)
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {"default": {"class": "noqx.logging.LoguruHandler", "level": log_level}},
+    "loggers": {
+        "uvicorn.error": {"handlers": ["default"], "level": log_level},
+        "uvicorn.access": {
+            "handlers": ["default"],
+            "level": log_level,
+        },
+    },
+}
+
 if __name__ == "__main__":
     environ["DEBUG"] = "TRUE" if args.debug else "FALSE"
-    uvicorn.run(app="noqx:app", host=args.host, port=args.port, reload=args.debug, log_level="debug" if args.debug else "info")
+    uvicorn.run(
+        app="noqx:app",
+        host=args.host,
+        port=args.port,
+        reload=args.debug,
+        log_level=log_level.lower(),
+        log_config=LOGGING_CONFIG,
+    )
--- noqx/logging.py
+++ noqx/logging.py
@@ -1,5 +1,26 @@
 """Logging utility for the site."""
 
+import inspect
 import logging
 
-logger = logging.getLogger("uvicorn.error")
+from loguru import logger
+
+
+class LoguruHandler(logging.Handler):  # pragma: no cover
+    """Emit standard logging to loguru logger."""
+
+    def emit(self, record: logging.LogRecord):
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        frame, depth = inspect.currentframe(), 0
+        while frame and (depth == 0 or frame.f_code.co_filename == logging.__file__):
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
+
+
+logger.remove()
