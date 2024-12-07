1faca20ec02573c9c54bd8e2a55a9734c6a66c0c
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2024-12-08 00:38:52 +0800

    feat: simplify railpool solver

--- solver/railpool.py
+++ solver/railpool.py
@@ -47,8 +47,7 @@ def solve(puzzle: Puzzle) -> List[Solution]:
         solver.add_program_line(area(_id=i, src_cells=ar))
         if rc:
             data = puzzle.text[rc]
-            if not isinstance(data, list):
-                data = [data]
+            assert isinstance(data, list), "Please set all NUMBER to tapa sub."
             for num in data:
                 if num != "?":
                     solver.add_program_line(f":- not area_len({i}, {num}).")
@@ -65,15 +64,7 @@ __metadata__ = {
     "category": "loop",
     "examples": [
         {
-            "data": "m=edit&p=7ZRPa9tMEMbv/hRlz3vYnV3r3yWkadyL6zaNX0IQIjiuQ0zt+m0clyLj755nZseVW1JKCYQcitDop0czu8+OVlp/3UzuZrbAEQrrrMcRIslJrpTT6TGe3y9m1St7vLm/Xd0BrH0/GNibyWI969Wa1fS2bVm1Z7Z9W9XGG2sIpzeNbc+qbfuuake2PccjYwtow5REwNMOL+Q500kSvQOPwBEMvARO53fTxexqmJQPVd2OreF5Xks1o1muvs2M+uD76Wp5PWfhy2Z5s1Bxvfm0+rzRNN/sbHv8e6fhMacs/upUE57s9Hpyj7avb+f/P2a3bHY7dPwjDF9VNXv/r8Oiw/NqiziqtoYyLg3wkl6LCUGEoyORIkvRs5QEycmizJeqJKUsuhQRvJMiTwdJ3uesdRI8eHFyKXEgkSSOYdS2QeIbiU5iX+JQck7h32el9bkzFWH8ftFxTtYX8MlcRHBfuQ/GoiWH9T0H1Go+1+boA3OGnHyfkyEfK5VxMFdRJi75y0jz4mrJU2LvD5h19ETyC7D6LHOLe2WMX6JHwvBZ6rxgcknHFbVpXvI8jrLLwGkc4cN8t9eRT+qT4IeSH1zB6pPwpeMN/fCvLGtU/0TgoOMEjBO1lv8S/dRDCuCoHCM49R9XcFoXBdb3HDDmQS3vQ8mH50znYo7a8wz9yXmN2AgXsh1OJEaJmWyTnHf7X30PT9+Rf7RTE97yTwdW8pz3Ta/Gb3nXM9+NnDV2vo3//tTP/6fm7ruXtj9fmh18MU3vAQ==",
-        },
-        {
-            "data": "m=edit&p=7VZdT9tKEH3Pr6j8vA/77Y83yoW+UHopVAhZEQKaiqigtIRUlaP8d87snsURVKqurqrygBzvHo9nzp4Zjzdefl9d3M2UsfJzjdLK4PCtT6erQzo1j5P5/c2se6N2VvfXizsApT7s76svFzfL2aSn13SyHtpuOFLDu66vTKUqi9NUUzUcdevhfTccquEYtyplYDvIThZwb4Sn6b6g3Ww0GvgQ2OewM8Cr+d3Vzez8IFv+7frhRFWyztsULbC6XfyYVdQh11eL28u5GC4v7pHM8nr+jXeWq8+Lryv6mulGDTtZ7vEv5LpRrsAsV9BTucznD8ttp5sNyv4Rgs+7XrR/GmEzwuNujfGwW1chpFDrpJ4KVQVlrZPNbVlc9hotTbZs+RhNp20qYz2NWzY4JNu2n69LxYslmOd0Vtuny1rzTK3FxRO51pKtmJC+SUU4S+N+Gm0aT1AjNbg0/pNGncaQxoPks4fSeeeUl1wsGB3elAAlgoMGxnoJ4y2KyC1hC9zQP8IHyRTs6ONq4OLTALfErfKe/B78nvwe/J48HvyBsQGxNbUFxNZ4zgXHwgPN29jTRzhD0Sw6Cw7AkVjyLZyoQyhrAUdq1tBjsh7XgEceHrFrMidm4OIDzZo8GvyW2gw0P2L4GPJb8JdaafBb8reoj846Bbs21wozcPFBrC6x4JQ+TZxSh7yWC9AmnS+4dtCZ18UMHvqAx7X0aeHTZh7MwLk+mIGppxE9xBE+DXkiYiNjIzgjOSM0R2qOiI3MBc8R18SNwvWomT2Qdu2Se9rBuW6N+tesfy050l/WLRi99Mgj2FOPh05PneiZRw3oVcdexQwf2tGrjr2KGZi5SCz7zVnxJzbgt+SRvyJ5lRNGLN7ejMFpyKkRKztAwtCgGauRF5+vQ2/gesSlT9A/0E0sesgj2BZtqJtl3azEkkf0FCwa6G/luVOPYMsewAyceTAzF2wkp2k72U2jT2NM20wtG/V/2sr//472Wzk9djn5Lnh+hFe7HNNJj8+ezaT6WaWzd/IV9fol9Je+hOQR6Jf2Er00OXitp5MH",
-        },
-        {
-            # This example has extra rules, which is not implemented by the current solver
-            # https://brokensign.com/puzzle/2022/08/04/rail-pool-forgetful.html
-            "data": "m=edit&p=7Zddbxs3E4Xv/SuKvebF8ms/dFOkadwb120aF0EgCIHjuohRB27juChk+L/nGfJQWsgBihdF3+YikLRzRJGcM2dmuKvbP+7O3186n+wdJ9c7zyuPffmkcSifXq+zqw/Xl6uv3JO7D29v3gOc++H42P16fn17ebTWrM3R/XZebZ+77Xerdec71wU+vtu47fPV/fb71fbUbV/wU+c8Yyd1UgA+28OX5XdDT+ug78Gn4FSXvQJeXL2/uL58fVJHflytt2euMz/flNUGu3c3f1524mHfL27evbmygTfnHwjm9u3V7/rl9u6Xm9/uNNdvHtz2SaX74hN0456uwUrX0CFdxfMv0503Dw/I/hOEX6/Wxv3nPZz28MXqnuvp6r5Lviz9GjIOTdkwxRZ7G5gOp+T5YMowHAxMfV2zWDTXfRcj3qeDZT7WkeWk9HgrP2SNLQdHTVwMTXXeYmQOByOhr9wXy0L/KOLgpdNyLI6PWYSkCJZjo+guh+raxchUme0mkSBf0vSqXI/LNZTrGVl021iu35ZrX665XE/KnGckN6bBxQFCwXVYGhsfhufZpR65Cp4c33c4efQAY12ydBiOCUwQBWdwncN+u32wrEW4spZ9NAfrUtY+mX3GygHr0qy1M+eMiQ7GuhzqWqzLJqjhlFwe6lqsy1ZihifWzvI7+sX+dm6RtYYnzZnY33JefLF/TyYKHvccUlz4xZf1RMOD/A52Nmr+yJ6zNDGdrT4LzuA2brmovrDguhYLlv7D7KKVsWE4R/HHgpU7NNxhNIzSEEsuFCMapr7lFE0aHiPztWeG21jjKvOlOdYlxY51SZpj0VD5QvPUNKdmsmoG67JqButyVr4yuimuck9RLrBuaLmgTrJ1VMOqGSxrtQ/xZsWLBddY6n1KOJO7hqlVvosb+VUNF56++koT+qj+0xCJUTWTqPNB+hhO0oeeSuopLPOlD72QWy/QXw2X+tSeWLBisfmTuE1wnhTvRLx2bhUM/0n6THCepM9ELNKw1Jj2wZJr8e+JyysuT79LWyz9qF6mBlKrAWojSU8s/OtaLP0ovwG/SdzQJEsf7C7GUquT6m2yc0Z+6a8U5DeYnk1b/A7yO1j/ivPI2llrZ/z6ljv8RvmN+M0t73BWH2F3WkU0jNIQC27j8OnFp4dPEJ8AH7srFm7wsZtd0Qo+dmoXbqydtXaGj281Bp/Yag8+SXwSfNT72F1+jVvqWw/iN8hvwG+U34jfLL8Zv3YzKRxYa/fUwgG/docqHPAb5DfgN8lvwq/OJeyu9kptBNVtoK8bB54C9xzwG1u/W7+o5mc7wxV7sHO7+cVX0HzTM6hfAv3SaoDzc5d307adq6ahPRgUTC+0vEf6ZZd3YozqI9M2a22m11QPRTd7dih5xFfLqek2qpfLc630Md289vT4Uk5jsvNZ9cM51s5n699h1+MDWDHaOdz6yOq5nZ/UcD0zuTm/LLfop+WaynUot+7RHs/+pwe4f/6U8Ld01lSC/Rv49Ct/+e3/9dvmaM2fqoej7q+ufNaR0fTlf9Z/9D/LUtB/bs36udHh+NgcfQQ=",
+            "data": "m=edit&p=7ZRNaxsxEIbv/hVBZx1WI3m/LsFNnV5c9yMuISxLsJ1NY2rHrZ0tZY3/e94Zyd1tMJQSCDmUZWcfjWdGr8aStj/q6abSKR6b6kgbPNaRvBRl8kbhmSwellV+ogf1w916A9D6w/m5vp0ut1WvCFFlb9dkeTPQzbu8UEZpRXiNKnXzKd817/NmqJsL/KR0Ct/IBxFw2OKl/M505p0mAo/BDgy8As4Xm/myuh55z8e8aCZa8TxvJJtRrdY/KxV08Hi+Xs0W7LivV7fL4NzWN+tvdQgz5V43A690dESpPaaUnU+VhoBnK51NH9D27d3i+zG5Wbnfo+OfIfg6L1j7lxbTFi/yHew43ymKOdVCi0Y3Uc9acZyeti5n2NVxxE7m62Rl6ZMQE0mSoU6QMQn7Whc0GFFyJfZcLImdQKhurNi3YiOxfbEjiRlCv4kzbZJI5YT6/bTlhLRJoZM5deB+4D4Yi5YY9h/YIjfEc26CPjDHiEkOMTHisVKpg7nSzHPGJ8PPi68mQ56N6TD70ROJT8FBZ5ZojAOjfoYeCUNnFuYFU+T9+CLXz0uG6wSOYrCvI9yNjw5+xFPQSdBDXg++4KCTcNLxD/3WH1jWGPQTgW2oY1HHhVy+Jfq+h2TBLrBzYN9/fMF+XWTZf2CLmp1c3ocSD81xmIvZhZ7H6E/Ca8RGuJTtcCbWiY1lmyS82//pPDx/R/5VTkH4l/94sJKXHJe9Qo0W99XJeL1ZTfnaG9587YzG9WpWbQ5j3OD7nvql5C1wSLT7f6m//KXO3Y9e21Z+bXJwuMreIw==",
         },
     ],
 }
