760d427826c18a3d92c83d340182f41a43cc5769
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2024-12-31 14:38:08 +0800

    perf: slightly optimize araf solver with more examples

--- solver/araf.py
+++ solver/araf.py
@@ -42,20 +42,20 @@ def solve(puzzle: Puzzle) -> List[Solution]:
 
     for (r, c), num in puzzle.text.items():
         assert isinstance(num, int), "Clue must be an integer."  # ignore clues with ?
+
+        exclude = []
+        for (r1, c1), num1 in puzzle.text.items():
+            assert isinstance(num1, int), "Clue must be an integer."  # ignore clues with ?
+            if abs(r - r1) + abs(c - c1) >= max(num, num1):
+                exclude.append((r1, c1))
+
+            if (r, c) != (r1, c1) and abs(num - num1) <= 1:
+                exclude.append((r1, c1))
+
         solver.add_program_line(f"clue({r}, {c}, {num}).")
-        solver.add_program_line(grid_src_color_connected((r, c), color=None, adj_type="edge"))
+        solver.add_program_line(grid_src_color_connected((r, c), exclude_cells=exclude, color=None, adj_type="edge"))
         solver.add_program_line(araf_region_clue_count((r, c)))
         solver.add_program_line(araf_region_count((r, c)))
-
-    tag = tag_encode("reachable", "grid", "src", "adj", "edge", None)
-    for (r1, c1), num1 in puzzle.text.items():
-        for (r2, c2), num2 in puzzle.text.items():
-            assert isinstance(num1, int) and isinstance(num2, int)
-            if abs(r1 - r2) + abs(c1 - c2) >= max(num1, num2):
-                solver.add_program_line(f":- {tag}({r1}, {c1}, {r2}, {c2}).")
-
-            if abs(r1 - r2) + abs(c1 - c2) == 1 and abs(num1 - num2) <= 1:
-                solver.add_program_line(f":- {tag}({r1}, {c1}, {r2}, {c2}).")
 
     for r, c, d in puzzle.edge:
         solver.add_program_line(f":- not edge_{d.value}({r}, {c}).")
@@ -77,6 +77,7 @@ __metadata__ = {
         {
             "data": "m=edit&p=7VRRb9owEH7nV1R+9oPPCcHJG+tgL4xua6eqiiIUaLqigdIFMk1G/PfeXcIcb7BpQur2MIWcvs93dr4zd7f5UudVIWN8AiOVBHwCo/g1If1U+9wst6siuZDDevtYVgikvBqP5UO+2hS9tI3KejsbJ3Yo7ZskFVpIfkFk0r5PdvZtYqfSXqNLSMC1CSIQUiMcOXjLfkKXzSIoxNMGRwjvEC6W1WJVzCbNQe+S1N5IQd95xbsJinX5tRDNNuaLcj1f0sI832Iym8flU+vZ1Pfl57qNhWwv7bCROzoiN2jlhg1s5BI6IpeyOFtucf+p2NTzY1rjbL/HO/+AamdJSsI/OmgcvE52aKfJTmiDW0MZNX+L0LFHA0AaOap9b+DTPlJwNPK8IQXHjg482g+RBo7SUR3qi4x8VRGpct+NKAVHB/53jfIped2HDMnoeElG31GS4U6OKdipAkVHOzcougAnE5SfBQDpHnQ4ndfZr+k83eEU78SB/jHev3AI/GSA/xCXDQSk5+DHcgAuiju2Y7aa7Q3WjLQB29dsFds+2wnHjNjesr1kG7KNOGZAVfdHdXm+HKw/zDc2mB2OLYix3gKsbxVLDXixhEEhxlo6YI3FgBiMkVo162AGiCn+t+ml2vC87D79f2sl66VihNPjYlpW63yFE2Rar+dFdeA4r/c98U3wi20BMvw/wv/GCKf7Vy/cMOf2b4pX+73XpL2S4qme5bNFiXWG98fuQ/udcrcd+Ss3NukJ96FvT7qbVj7uxmnxk+PFLxiHiMir/EFkvWc=",
         },
+        {"url": "https://puzz.link/p?araf/10/10/1h4h1h7i6h6heldh34n5icrai8nc9hblahah3ibh-32h-32hd", "test": False},
         {"url": "https://pzplus.tck.mn/p?araf/10/11/1l6p7q4467h5g55q7g7647g1q-108g-10h-1075-10q9p-10ld", "test": False},
     ],
 }
