2c7edc4b7871d63f2c0a5237c7f793fdb9dfb88e
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2025-03-01 03:00:18 +0800

    fix: JSProxy conversion error with parameters

--- main_pyscript.py
+++ main_pyscript.py
@@ -1,11 +1,18 @@
 """Entry point for the noqx-pyscript connector."""
 
+from typing import Any, Dict
+
 from pyscript import window  # pylint: disable=import-error  # type: ignore  # noqa: I001
 
-from noqx.manager import prepare_puzzle, generate_program, store_solution, load_solvers
+from noqx.manager import generate_program, load_solvers, prepare_puzzle, store_solution
 
 load_solvers("solver")
 
-window.prepare_puzzle = prepare_puzzle
+
+def _prepare_puzzle(puzzle_name: str, puzzle_content: str, param: Dict[str, Any]):
+    return prepare_puzzle(puzzle_name, puzzle_content, param.to_py())
+
+
+window.prepare_puzzle = _prepare_puzzle
 window.generate_program = generate_program
 window.store_solution = store_solution
