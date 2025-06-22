8a15a94f4ff524123fd7d5a24c9b1d18b6fd0164
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2025-06-22 14:22:44 +0800

    chore(deps): upgrade pyscript to `2025.5.1`

--- penpa-edit/js/prepare_deployment.js
+++ penpa-edit/js/prepare_deployment.js
@@ -3,13 +3,13 @@ const ENABLE_DEPLOYMENT = false;
 if (ENABLE_DEPLOYMENT) {
   const script = document.createElement("script");
   script.type = "module";
-  script.src = "https://pyscript.net/releases/2025.2.4/core.js";
+  script.src = "https://pyscript.net/releases/2025.5.1/core.js";
   script.async = false;
   document.head.appendChild(script);
 
   const link = document.createElement("link");
   link.rel = "stylesheet";
-  link.href = "https://pyscript.net/releases/2025.2.4/core.css";
+  link.href = "https://pyscript.net/releases/2025.5.1/core.css";
   document.head.appendChild(link);
 
   const user_script = document.createElement("script");
