93f1da8610bfce8661f99fb3b427f42c9b86c18f
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2025-12-11 23:35:22 +0800

    feat: limit the neighbor adjacency to 4 in `general_shape`

--- noqx/rule/shape.py
+++ noqx/rule/shape.py
@@ -40,13 +40,18 @@ OMINOES = {
 
 
 def canonicalize_shape(shape: Iterable[Tuple[int, int]]) -> Iterable[Tuple[int, int]]:
-    """
-    Given a (possibly non-canonical) shape representation,
+    """Convert a shape to its canonical representation.
 
-    Return the canonical representation of the shape, a tuple:
-        - in sorted order
-        - whose first element is (0, 0)
-        - whose other elements represent the offsets of the other cells from the first one
+    * The representation of a shape containing all the cells that consist of the shape, and:
+        * the first element can be any coordinate,
+        * the other element represent the offsets of the other cells from the first one.
+
+    * The **canonical** representation of the shape is a sorted tuple, and:
+        * the first element is `(0, 0)`,
+        * the other elements represent the offsets of the other cells from the first one.
+
+    Args:
+        shape: the representation of a shape.
     """
     shape = sorted(shape)
     root_r, root_c = shape[0]
@@ -57,25 +62,22 @@ def canonicalize_shape(shape: Iterable[Tuple[int, int]]) -> Iterable[Tuple[int, 
 def get_variants(
     shape: Iterable[Tuple[int, int]], allow_rotations: bool, allow_reflections: bool
 ) -> Set[Iterable[Tuple[int, int]]]:
-    """
-    Get a set of canonical shape representations for a (possibly non-canonical) shape representation.
+    """Generate the equivalent variants for a shape.
 
-    allow_rotations = True iff shapes can be rotated
-    allow_reflections = True iff shapes can be reflected
+    Args:
+        shape: the representation of a shape.
+        allow_rotations: Whether the shapes can be rotated to build the variants.
+        allow_reflections: Whether the shapes can be reflected to build the variants.
     """
-    # build a set of functions that transform shapes
-    # in the desired ways
     functions = set()
     if allow_rotations:
         functions.add(lambda shape: canonicalize_shape((-c, r) for r, c in shape))
     if allow_reflections:
         functions.add(lambda shape: canonicalize_shape((-r, c) for r, c in shape))
 
-    # make a set of currently found shapes
     result = set()
     result.add(canonicalize_shape(shape))
 
-    # apply our functions to the items in this set
     all_shapes_covered = False
     while not all_shapes_covered:
         new_shapes = set()
@@ -97,28 +99,34 @@ def general_shape(
     adj_type: Union[int, str] = 4,
     simple: bool = False,
 ) -> str:
+    """A rule to define general shapes in a grid or an area.
+
+    * Two predicates will be generated, `shape` and `belong_to_shape`. The `shape` predicate
+    defines the shape pattern, while the `belong_to_shape` predicate defines whether a cell
+    belongs to a certain shape instance.
+
+    Args:
+        name: The name of the shape.
+        _id: The ID of the shape, needs to be unique.
+        deltas: The relative coordinates of the shape cells.
+        color: The color to be checked.
+        _type: The type of the shape rule (accepted types: "grid" or "area").
+        adj_type: The type of adjacency (accepted types: `4`, `8`, `x`, `loop`, `loop_directed`).
+        simple: Whether to skip the adjacency re-checking.
+
+    Warning:
+        Although the shape representation does not require the connectivity of the shape,
+        it is recommended to ensure that the provided shape is connected. Some derived rules
+        may behave weird if the shape is not connected.
+
+    Warning:
+        The `simple` option is more efficient, but the use-case is limited. It is only recommended to use
+        in the `area` type, and every area only contains **one piece** of the shape.
     """
-    Generates a rule for general shapes (using bruteforce technique).
-    The deltas are the relative coordinates of the shape cells.
 
-    A grid rule and an adjacent rule should be defined first.
-    """
-
-    def get_neighbor(r: int, c: int, _type: Union[int, str] = 4) -> Iterable[Tuple[int, int]]:
-        """Get the neighbors of a cell."""
-        shape_4 = ((r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1))
-        shape_x = ((r - 1, c - 1), (r - 1, c + 1), (r + 1, c - 1), (r + 1, c + 1))
-
-        if _type == 4:
-            return shape_4
-
-        if _type == "x":
-            return shape_x
-
-        if _type == 8:
-            return shape_4 + shape_x
-
-        raise ValueError("Invalid type, must be one of 4, 8, 'x'.")
+    def get_neighbor(r: int, c: int) -> Iterable[Tuple[int, int]]:
+        """Get the 4-directional neighbors of a cell."""
+        return ((r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1))
 
     validate_type(_type, ("grid", "area"))
     if not deltas:
@@ -126,7 +134,6 @@ def general_shape(
 
     tag = tag_encode("shape", name, color)
     tag_be = tag_encode("belong_to_shape", name, color)
-    neighbor_type = adj_type if adj_type in [4, 8, "x"] else 4
     data = ""
 
     variants = get_variants(deltas, allow_rotations=True, allow_reflections=True)
@@ -147,7 +154,7 @@ def general_shape(
                     f"{tag_be}(A, R + {dr}, C + {dc}, {_id}, {i}) :- area(A, R + {dr}, C + {dc}), {tag}(A, R, C, {_id}, {i})."
                 )
 
-            for nr, nc in get_neighbor(dr, dc, _type=neighbor_type):
+            for nr, nc in get_neighbor(dr, dc):
                 if (nr, nc) in variant:
                     if adj_type not in [4, 8, "x"] and (dr, dc) < (nr, nc):
                         valid.add(f"adj_{adj_type}(R + {dr}, C + {dc}, R + {nr}, C + {nc})")
@@ -170,10 +177,12 @@ def general_shape(
 
 
 def all_shapes(name: str, color: str = "black", _type: str = "grid") -> str:
-    """
-    Generate a constraint to force all {color} cells are in defined shapes.
+    """A rule to ensure all the color cells belong to defined shapes in a grid or an area.
 
-    A grid rule and a shape/belong_to_shape rule should be defined first.
+    Args:
+        name: The name of the shape.
+        color: The color to be checked.
+        _type: The type of the shape rule (accepted types: "grid" or "area").
     """
     validate_type(_type, ("grid", "area"))
     tag = tag_encode("belong_to_shape", name, color)
@@ -195,7 +204,8 @@ def count_shape(
     color: str = "black",
     _type: str = "grid",
 ) -> str:
-    """
+    """A rule to compare the number of certain shapes to a specified target.
+
     Generates a constraint to count the number of a shape.
 
     A grid rule and a shape rule should be defined first.
@@ -210,7 +220,7 @@ def count_shape(
         rule = f":- {{ {tag}(R, C, {_id}, _) }} {rop} {num}."
 
     if _type == "area":
-        rule = f":- area(A, _, _), {{ {tag}(A, R, C, _, {_id}) }} {rop} {num}."
+        rule = f":- area(A, _, _), {{ {tag}(A, R, C, {_id}, _) }} {rop} {num}."
 
     return rule
 
