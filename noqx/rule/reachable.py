8d3d8fae1a7e73dc70506fb50db611a5c83aca10
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2025-12-11 18:18:06 +0800

    docs: add documents for connectivity rules

--- noqx/rule/reachable.py
+++ noqx/rule/reachable.py
@@ -1,6 +1,11 @@
-"""Utility for reachable things and connectivity tests."""
+"""Generate rules for reachable things and connectivity tests.
 
-from math import log2
+Note:
+    Every connectivity rule consists of three parts: **initialization**, **propagation**
+    and **constraint** (optional). This structure is similar to the flood-fill algorithm,
+    and it is recommended by the [Clingo](https://potassco.org/clingo/) documentation.
+"""
+
 from typing import List, Optional, Tuple, Union
 
 from noqx.rule.helper import tag_encode, target_encode, validate_type
@@ -9,10 +14,16 @@ from noqx.rule.helper import tag_encode, target_encode, validate_type
 def grid_color_connected(
     color: str = "black", adj_type: Union[int, str] = 4, grid_size: Optional[Tuple[int, int]] = None
 ) -> str:
-    """
-    Generate a constraint to check the reachability of {color} cells.
+    """A rule to ensure all the color cells are connected in the grid.
 
-    An adjacent rule and a grid fact should be defined first.
+    * This is the most efficient connectivity checker in this module, since it only considers a global
+    constraint. If the problem can be modelled to use this rule, don't hesitate to use it.
+
+    Args:
+        color: The color to be checked.
+        adj_type: The type of adjacency (accepted types: `4`, `8`, `x`, `loop`, `loop_directed`).
+        grid_size: The size of the grid in (`rows`, `columns`). If provided, the propagation
+                   starts from the middle of the grid to increase the speed potentially.
     """
     validate_type(adj_type, (4, 8, "x", "loop", "loop_directed"))
     tag = tag_encode("reachable", "grid", "adj", adj_type, color)
@@ -20,7 +31,6 @@ def grid_color_connected(
     if grid_size is None:
         initial = f"{tag}(R, C) :- (R, C) = #min{{ (R1, C1): grid(R1, C1), {color}(R1, C1) }}."
     else:
-        # propagation from the middle of the grid to increase the speed
         R, C = grid_size
         initial = (
             f"{tag}(R, C) :- (_, R, C) = #min{{ (|R1 - {R // 2}| + |C1 - {C // 2}|, R1, C1): grid(R1, C1), {color}(R1, C1) }}."
@@ -32,10 +42,17 @@ def grid_color_connected(
 
 
 def border_color_connected(rows: int, cols: int, color: str = "black", adj_type: Union[int, str] = 4) -> str:
-    """
-    Generate a constraint to check the reachability of {color} cells connected to borders.
+    """A rule to ensure all the color cells are connected to the borders of the whole grid.
 
-    An adjacent rule and a grid fact should be defined first.
+    * Similar to `grid_color_connected`, this is also a global constraint. The different is that
+    the propagation starts from the borders of the grid. Moreover, the color cells does not need
+    to be connected *inside* the grid.
+
+    Args:
+        rows: The number of rows in the grid.
+        cols: The number of columns in the grid.
+        color: The color to be checked.
+        adj_type: The type of adjacency (accepted types: `4`, `8`, `x`).
     """
     validate_type(adj_type, (4, 8, "x"))
     tag = tag_encode("reachable", "border", "adj", adj_type, color)
@@ -47,10 +64,13 @@ def border_color_connected(rows: int, cols: int, color: str = "black", adj_type:
 
 
 def area_color_connected(color: str = "black", adj_type: int = 4) -> str:
-    """
-    Generate a constraint to check the reachability of {color} cells.
+    """A rule to ensure all the color cells are connected in every area.
 
-    An adjacent rule and an area fact should be defined first.
+    * The complexity of this rule is based on the number of areas instead of the whole grid.
+
+    Args:
+        color: The color to be checked.
+        adj_type: The type of adjacency (accepted types: `4`, `8`, `x`).
     """
     validate_type(adj_type, (4, 8, "x"))
     tag = tag_encode("reachable", "area", "adj", adj_type, color)
@@ -67,11 +87,22 @@ def grid_src_color_connected(
     color: Optional[str] = "black",
     adj_type: Union[int, str] = 4,
 ) -> str:
-    """
-    Generate a constraint to check the reachability of {color} cells starting from a source.
+    """A rule to collect all the color cells that are reachable to a source cell in a grid.
 
-    An adjacent rule and a grid fact should be defined first.
-    If adj_type is "edge", an edge fact should be defined first.
+    * This rule is a local definition, and only does the collection part. To further utilize the rule,
+    a `count_reachable_src` rule may be applied to count the size of the whole connected region.
+
+    * This rule does not ensure that every cell belongs to the connected part of the source cell,
+    a `avoid_unknown_src` rule may be applied to ensure this constraint.
+
+    * The complexity of this cell is mainly based on the size of cells connected to the source cell.
+
+    Args:
+        src_cell: The source cell in (`row`, `column`).
+        include_cells: The list of cells to be included as reachable cells to the source cell.
+        exclude_cells: The list of cells to be excluded from reachable cells to the source cell.
+        color: The color to be checked. If it is `None`, only the `edge` adjacency is accepted.
+        adj_type: The type of adjacency (accepted types: `4`, `8`, `x`, `edge`, `loop`, `loop_directed`).
     """
     if color is None:
         validate_type(adj_type, ("edge",))
@@ -102,10 +133,20 @@ def grid_src_color_connected(
 
 
 def bulb_src_color_connected(src_cell: Tuple[int, int], color: Optional[str] = "black", adj_type: Union[int, str] = 4) -> str:
-    """
-    Generate a constraint to check the reachability of {color} cells starting from a bulb.
+    """A rule to collect all the color cells that are orthogonally connected to a source cell in a grid.
 
-    An adjacent rule and a grid fact should be defined first.
+    * The rule behaves like a **bulb**. It starts from the source cell and spread orthogonally until it hits
+    a cell without a specified color or the border of the grid, which like a **wall** in reality.
+
+    * Since the connection pattern is limited in this rule, it is more efficient than `grid_src_color_connected`
+    with constraints. A typical use-case is to calculate the size of a rectangle: instead of spreading from the
+    source cell to the whole grid, it is possible to get the height and width of the rectangle only, which greatly
+    simplifies the grounding process.
+
+    Args:
+        src_cell: The source cell in (`row`, `column`).
+        color: The color to be checked. If it is `None`, only the `edge` adjacency is accepted.
+        adj_type: The type of adjacency (accepted types: `4`, `edge`).
     """
     if color is None:
         validate_type(adj_type, ("edge",))
@@ -135,10 +176,19 @@ def count_reachable_src(
     color: Optional[str] = "black",
     adj_type: Union[int, str] = 4,
 ):
-    """
-    Generate a constraint to count the reachable cells starting from a source.
+    """A rule to compare the number of reachable cells to the source cell with a specified target.
 
-    A grid_src_color_connected or bulb_src_color_connected should be defined first.
+    * This rule is often used together with `grid_src_color_connected` or `bulb_src_color_connected`.
+
+    Args:
+        target: The target number or a tuple of (`operator`, `number`) for comparison.
+        src_cell: The source cell in (`row`, `column`).
+        main_type: The main type of the reachable rule (accepted types: `grid` or `bulb`).
+        color: The color to be checked. If it is `None`, only the `edge` adjacency is accepted.
+        adj_type: The type of adjacency (accepted types: `4`, `8`, `x`, `edge`, `loop`, `loop_directed`).
+
+    Raises:
+        ValueError: If the main type is other than `grid` or `bulb`.
     """
     if color is None:
         validate_type(adj_type, ("edge",))
@@ -158,10 +208,14 @@ def count_reachable_src(
 
 
 def avoid_unknown_src(color: Optional[str] = "black", main_type: str = "grid", adj_type: Union[int, str] = 4) -> str:
-    """
-    Generate a constraint to avoid cells starting from unknown source.
+    """A rule to avoid all the cells being unreachable to any source cell.
 
-    Use this constraint with grid_src_color_connected.
+    * This rule is often used together with `grid_src_color_connected` or `bulb_src_color_connected`.
+
+    Args:
+        color: The color to be checked. If it is `None`, only the `edge` adjacency is accepted.
+        main_type: The main type of the reachable rule (accepted types: `grid` or `bulb`).
+        adj_type: The type of adjacency (accepted types: `4`, `8`, `x`, `edge`, `loop`, `loop_directed`).
     """
     if color is None:
         validate_type(adj_type, ("edge",))
@@ -175,12 +229,15 @@ def avoid_unknown_src(color: Optional[str] = "black", main_type: str = "grid", a
 
 
 def grid_branch_color_connected(color: Optional[str] = "black", adj_type: Union[int, str] = 4) -> str:
-    """
-    Generate a constraint to check the reachability of {color} cells with branches.
+    """A rule to collect all the color cells that are connected to any cells in a grid.
 
-    An adjacent rule and a grid fact should be defined first.
-    If adj_type is "edge", an edge fact should be defined first.
-    Unless no initial cells are given, please consider using grid_src_color_connected.
+    * This rule is similar to `grid_src_color_connected`, but it defines the connected branch
+    for every cell in a grid. Hence, this rule is **very inefficient** compared to other rules.
+    Unless no initial clue cells are given, please consider using `grid_src_color_connected` instead.
+
+    Args:
+        color: The color to be checked. If it is `None`, only the `edge` adjacency is accepted.
+        adj_type: The type of adjacency (accepted types: `4`, `8`, `x`, `edge`).
     """
     if color is None:
         validate_type(adj_type, ("edge",))
