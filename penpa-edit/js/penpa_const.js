35a664e0c660642a5ad7d0a30d7ccac31ab87ef0
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2026-01-01 18:59:32 +0800

    chore(deps): update the penpa-edit to v3.2.1

--- penpa-edit/js/penpa_const.js
+++ penpa-edit/js/penpa_const.js
@@ -18,5 +18,5 @@ const style_tag_cache = {
   constraints_settings_css: "link#constraints_settings_css",
 };
 
-const penpa_edit_hash = "d8020cbf0f07230ebe06067338c3afe6eff8ed39";
+const penpa_edit_hash = "aaa65640a869e75b840dbe81a5ef323fc8be3d08";
 const penpa_edit_prefix = `https://cdn.jsdelivr.net/gh/swaroopg92/penpa-edit@${penpa_edit_hash}/docs/`;
