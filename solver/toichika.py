db175df335f8ec88b57f47de4aa352e9a6778eeb
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2026-01-14 09:18:11 +0800

    fix: change the type of the toichika solver

--- solver/toichika.py
+++ solver/toichika.py
@@ -32,7 +32,7 @@ class ToichikaSolver(Solver):
     """The Toichika solver."""
 
     name = "Toichika"
-    category = "num"
+    category = "var"
     examples = [
         {
             "data": "m=edit&p=7VdrTyJJFP3ur5jU16lk69EvSPYDOjg7syPqqHGFENJiqziN7Tags23873Nu9W2hAZ1HnH0kG6DqcOr2vedWFXWLyZ+zOE+kVvS2kUSPl6cj9zFR4D6KX4ejaZo0X8nWbHqZ5QBS7m5vy/M4nSTy/cnlh62sdfem9cdtNO129Vs1e6eOr7avXn8c//5uZHO93Yn2dvZ2Ruai9dvW5n7Qfh3szSZH0+R2f6w3r466h+d7xxcN81e70/WK7q7y33fPf7ltHf260WMN/Y37otEsWrJ42+wJLaQw+GjRl8V+877YaRZtWRxgSMioL8V4lk5HwyzNcuE4DbsP5YMGsD2Hx26c0FZJagXcYQx4AhjneXY36Aw2S2qv2SsOpaDgm+5xgmKc3SYUjcTR92E2Ph0RcRpPMX+Ty9GNkBYDk9lZ9mnGprr/IItWmcLJN6YAJ1UKBMsUCK1JgTJbTOH45VNo9B8esDwfkcSg2aN8juYwmsOD5j3ajmu1a0+a98JquAnl0hyLwIL2V2itAvB2lbfE6yUeIbZdIOPaQ+iQhXXtG9cq1/qu/eBs2pBkjC+NgUODbWbwWzAh41AaD3oJe2qObQNYlVh7sI/YHr8lz7C9kcZnnz78P2LY+/4cB/xsgGeDBmP4D9l/iLiRV+LILmD4jzBpDmtgjutDf/VsBP8Rx4qgIWIN7vfOOYbkk7FPcdmnR5pZj4V/j/1ozAmtocuX5qGyhx+/8gObkG1CPBtW2pBjhT2aB87d0txyXuTTsn6Ka9neIl+P9XvQ73EsD7ECjhVQLNYZUr6sP4LPRjWfeLbCAXyGlU+a/0obrWP1LPxHrDOis7LkrWpIq0s/6KU1pX700trSHj0w22OPPWKtYV/qdFiXcdGDL/WgBy5zRC8t7z300lb7h9a0wTob0KNYj4IexfaK/FexoO0RW2DWTLja88jRKtajoEezHg09vC4WazTHsDdVjsC8/9EDl3sDPTDHwrpbXnfnU3OO2FeW9xV62LNOsq8w6VHVnqd8GTcoX/avKMfKP80/68H+sbx/0ANTXBwAx+4Y2HKt59rAHQ8hHWLfeMwJEhitOewo6cVTzZ3GP3ZKfVVqD78iqur1l//f4/obPXEwy8/jYYK61D67SF51snwcp/jWmY1Pk3z+/eAyvkkEbgtikqWDSfnUIPkcD6eiWV5YFkdq3LXzVaPSLLtJR9frPFRDNXJ0cZ3lydohIhNof8IVDa1xdZrlZ0ua7uI0refiLnM1ajjKh2mdmuao5Avf3RasMeN4elkjFqp+zVNyvTSZ07guMf4UL0Ubz6fjYUN8Fu6DOm5oef+/2v2rr3a0VOqHL3g/6XT7ipweZhy3l2JXipvZIB5gtgX+RcjneFT2f4THjeu7dL4U/1Tcp3RqXB2+bwB3xp/Ley/BH6z6D/t/+352x2SWP1Oz5oPL9JrKBfaZ4rUwuo5/ok4tjC7zK0WJxK7WJbBrShPY5eoEarVAgVypUeCeKFPkdblSkarlYkWhVuoVhVosWb3+xhc=",
