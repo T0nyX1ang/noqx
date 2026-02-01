0abfb170109793769f1280411dfce386c8eef4e2
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2026-02-02 00:52:30 +0800

    test: fix coverage issue with arrowflow

--- solver/arrowflow.py
+++ solver/arrowflow.py
@@ -26,7 +26,7 @@ class ArrowFlowSolver(Solver):
     category = "var"
     examples = [
         {
-            "data": "m=edit&p=7Vbvb7JIEP7uX9Hs125yLChFkvugVt9732uprRpPiTFoUWnB9RBsD+P/3plFC4u0uTS5X8kFd5h5Zpid2V0e3P4eO6FLmYI/zaBwh6vKDDFUQxdDOV59L/Jd84I24mjFQ1Aovet06MLxty79MVrdtHjj5brx286IxmP2TYm/K8OnztPlQ/Drd08LWccyurfdW09dNn5pNe/19qXejbeDyN3dB6z5NBj3F93hsq7+0bbG1WR8p9R+jBc/7RqDnyv2sYZJZZ/UzaRBk2+mTRihRIXByIQm9+Y+uTUTiyY9cBFanVASxH7kzbnPQyIwBnE36YMqqO1MHQo/aq0UZAro1lEHdQSqE4b8ZWpNmynUNe2kTwlO3hSPo0oCvnNxNiwO7TkPZh4CMyeC9duuvA2hGji28SN/jo+hbHKgSSNtYXRqASb5rAVIcmoB1bQF1EpawM7yLQy/1oK/4SXF1yeHA2zMA5Q/NW3sZJCpRqb2zD0xGDGrlNS19FYVN1bT4Q4RFkRoEGFX4RyKfSUahNhaZl6BabybuiIF65A2F6xjMJ7uo23ANDm3UQfz6t2sq5KXKZhMzdn4NL4gJxsfZzlANQCoZbaGCXMJtJqcoIbxembrcudMx9ZP8bA6zNyDHOEqYtuASycSfB0RoQrZh0WniSbktZCKkDUhb0RMW8ihkC0hq0LqIuYKt+1Pbmy6eVKRGhZZy4oUZ+4vKtLWUu6SL9yL/xg2qdikF4cLZ+7C22fFwcwNLyweBo4Pdm/lbFwCLEi23J9u07ip++rMI2KmRJz3SNha5JIgn/ON763LMpxcEugt1zx0S10Iuo/Lj1KhqyTVjIePhZpeHN+XexEfKQmae+Hcl6EoBIbK2eLQSUjgRCsJyBGylMldFxYzcuQSnWenMFuQLcehQl6JGMAl8PL//8n6136ycJOUL/PbP0O3Nqw1cF1yR8kmnjpTWGcC/4v+9irFsefhJxyUOYtwCRMB+gkZ5bxl+Ae8k/MW8TOSwWLPeQbQEqoBtMg2AJ0TDoBnnAPYB7SDWYvMg1UVyQenOuMfnCpPQTZZx6E34zGZVN4A",
+            "data": "m=edit&p=7Vb/b6pIEP/dv+Jlf+0mx4JSJLlc1Kfv+q6l+tR4SoxBi0oLrodg32H839/MooVF2jRN7ltyQYaZzwyzM7Pwwd0fsRO6lCn40wwKVziqzBCnaujiVE7HwIt81/xEG3G05iEolN53OnTp+DuXfh2vb1u88fy58fveiCYT9kWJb5TRY+fx6lvw242nhaxjGd277p2nrhq/tpo9vX2ld+PdMHL3vYA1H4eTwbI7WtXVP9vWpJpM7pXa18nyp31j+HPFPtUwrRySupn0aPLFtAkjlKhwMjKlSc88JHdmYtGkDy5Cq1NKgtiPvAX3eUgExiDuNr1RBbWdqSPhR62VgkwB3TrpoI5BdcKQP8+sWTOFuqadDCjBxZvidlRJwPcurobFob3gwdxDYO5EML/d2tsSqoFjFz/wp/gUyqZHmjTSFsbnFmCRt1qAJOcWUE1bQK2kBews38LoYy34W15SfH16PMLGfIPyZ6aNnQwz1cjUvnkgBiNmlZK6ll6q4sJqOlwhwoIIDSLsKjyHYl+JBiG2lpnXYBovpq5IwTqkzQXrGIxP98k2YJmc26iDef1i1lXJyxRMpuZsvFsM9gXBBAzfmROgGgDUMlvDlLkUWg1T/JJLUcM79MzW5e6Zju2fV4AJMfMAcoyTxNYBl55K8HVEhCrkAAZPE03Iz0IqQtaEvBUxbSFHQraErAqpi5hr3Lp3bm66gVKRGhZZy4oUz91fVKStpfwlH7gb/zFsWrFJPw6XzsKFN9CKg7kbfrJ4GDg+2P21s3UJMCHZcX+2S+Nm7ndnEREzJeO8R8I2IpcE+ZxvfW9TluHskkBvteGhW+pC0H1YvZYKXSWp5jx8KNT07Pi+3Iv4UEnQwgsXvgxFIbBUzhYPnYQETrSWgBwpS5ncTWGYkSOX6Dw5hdWCbBzHCvlOxAl8Aq///5+tf+1nCzdJ+TC//TN0a8OsgeuSe0q28cyZwZwJ/DeiiMNX6v2Ov70v8aLw8A3WypxFuIS7AH2DvnLeMvwVpsp5i/gFLWGxl8wEaAk5AVrkJ4AuKQrAC5YC7BWiwqxFrsKqinSFS10wFi6VJy2bbOLQm/OYTCs/AA==",
         },
     ]
 
@@ -51,14 +51,18 @@ class ArrowFlowSolver(Solver):
             validate_direction(r, c, d)
             validate_type(label, "normal")
             self.add_program_line(grid_src_color_connected((r, c), adj_type="line_directed", color="grid"))
+            self.add_program_line(f"hole({r}, {c}).")
+
             if isinstance(num, int):
-                self.add_program_line(f"hole({r}, {c}).")
                 self.add_program_line(count_reachable_src(num + 1, (r, c), adj_type="line_directed", color="grid"))
 
         for (r, c, d, _), symbol_name in puzzle.symbol.items():
             validate_direction(r, c, d)
-            if symbol_name.startswith("arrow_N") and symbol_name.split("__")[1] in ["1", "3", "5", "7"]:
-                self.add_program_line(f"{symbol_name.replace('B', 'W')}({r}, {c}).")
+            fail_false(
+                symbol_name.startswith("arrow_N") and symbol_name.split("__")[1] in ["1", "3", "5", "7"],
+                f"Invalid symbol at ({r}, {c}).",
+            )
+            self.add_program_line(f"{symbol_name.replace('B', 'W')}({r}, {c}).")
 
         self.add_program_line(display(item="arrow_N_W__1", size=2))
         self.add_program_line(display(item="arrow_N_W__3", size=2))
