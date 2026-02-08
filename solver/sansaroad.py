b068e34d6636f984f7f1fdaee4cc926f46f925f7
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2026-02-08 20:17:13 +0800

    feat: add centered clues for sansaroad and tetrochain-k

--- solver/sansaroad.py
+++ solver/sansaroad.py
@@ -3,6 +3,7 @@
 from noqx.manager import Solver
 from noqx.puzzle import Color, Direction, Puzzle
 from noqx.rule.common import display, fill_line, grid, shade_c
+from noqx.rule.helper import fail_false
 from noqx.rule.neighbor import adjacent, count_adjacent, count_covering
 from noqx.rule.reachable import grid_color_connected
 from noqx.rule.route import single_route
@@ -16,7 +17,7 @@ class SansaRoadSolver(Solver):
     category = "shade"
     examples = [
         {
-            "data": "m=edit&p=7Zbfb7pIEMDf/Ssu+9pNjuWH/EjuwVrba0+trRpPCTGoqLQgHoL2MP7vnV1oFFia5vL93t1Dg0yGz6yzM7PsLLu/Yjt0MBHpT9KwgAlcsi6zW1IVdgvZNXAjzzF+wY04WgchKBg/dvHS9nYOfhiv282gcbhp/LnXosmE3AnxvTB6uX25evb/uHelkNx2tV6n13HFVeP35vVTvXVV78W7YeTsn3xy/TKcDJa90UoX/251J3IyeRSUh8ny131j+FvNzEKwasdEN5IGTu4MExGEkQg3QRZOnoxj0jGSMU76YEJYszDyYy9y54EXhIgxAuPa6R9FUFtndcTsVGumkAigdzMd1DGoUegugsNm2klH9gwzGWBEJ79mf6cq8oO9Q2ejwdHneeDPXApmdgTl263dLcISGHbxIniNs6HEOuGkkabQ/2IK4OQjBaqmKVCNkwKNl6Ywd8O550zbqaMfmYFunU6wOs+Qw9QwaTrDs6qd1b5xBNllkjA5No5IkcANwYUSI6XOxRp/NBEVPpcIn6sil4tCBSd8/6IupDyrbr+fckmgXCxzIlfwLN0il7N8i1zRKrjO969W+FEr4lFpPDyu8v1olCscTuPkcJ2uC8e/ntW/xPn1kdl6lf3LIt+/LPLzlSV+vrLCX0e5zueKzM9XYW85h9dpPGWuCnQdOZy9P2WuEf68Gvf9hJ13y/afyOQAtidOJCZvmBSYVJhsszEtJkdMNpmUmayzMSrd4F9sAfBOIwMqp8AKaeV+8JNiMyWZHXDlS/nm9LJqJurH4dKeO9D8+2t76yA4c9Eu8Ka7lE+dN3seISM99i8tObaJ/ZkDZ9YF8oJg67kbnocPUw66q00QOlwThc5iVeWKmjiuZkG4KMR0sD0vnwv7IsqhdNfkEBwAuWc7DINDjvh2tM6Bi8Mz58nZFIoZ2fkQ7Ve7MJt/Lsepht4Qu00Jw1fc9wfS//sDia6U8I8/k/6blm1CwaFxJo8YbeOpPYVis3pRrpACl6x/PXq2J4LwkwZ1NhYxp00B/aRTXVh5vKIpXViLvNSBaLDlJgSU04eAFlsRoHI3AlhqSMAqehL1WmxLNKpiZ6JTlZoTneqyP5lW7R0=",
+            "data": "m=edit&p=7ZZbb+o4EIDf+RUrv9bSxrmQi7QPlNJuu0BpAbEQRShAgLQJYUMCPUH8946dVJDEqarV2ctDBRkN35jxzDgee/dXbIcOJiL9ShoWMIGPrMvskVSFPUL2GbiR5xi/4EYcrYMQFIwfu3hpezsHP4zX7WbQONw0/txr0WRC7oT4Xhi93L5cPft/3LtSSG67Wq/T67jiqvF78/qp3rqq9+LdMHL2Tz65fhlOBsveaKWLP1rdiZxMHgXlYbL8dd8Y/lYzsxCs2jHRjaSBkzvDRARhJMJDkIWTJ+OYdIxkjJM+mBDWLIz82IvceeAFIWKMwLh2+kcR1NZZHTE71ZopJALo3UwHdQxqFLqL4LCZdtKRPcNMBhjRya/Z36mK/GDv0NlocPT3PPBnLgUzO4Ly7dbuFmEJDLt4EbzG2VBinXDSSFPofzEFcPKRAlXTFKjGSYHGS1OYu+Hcc6bt1NHPzEC3TidYnWfIYWqYNJ3hWdXOat84guwySZgcG0ekSOCG4EKJkVLn4rqQ4iybfj/FGt+JTkeLpdFEVLjDiUT4XBW5XBQqOOH7F1k85eglgR+nROQKnhWnyOWsDEWuaBVc5/tXK/yoFfGoNB4eV/l+NMoVDqdxcrhO14XjX8/qX+L8+shsvcr+ZZHvXxb5+coSP19Z4a+jzN7aMldkfr4K2xMcXqfxlLkq0HXkcPb+lLlG+PNq3PcT9ukt260ikwPYzDiRmLxhUmBSYbLNxrSYHDHZZFJmss7GqLQdfLFhwDuNDKicAiuklbvHPxSbKcnsOCx/lG9OP1bNRP04XNpzB46K/treOghOaLQLvOku5VPnzZ5HyEgvCZeWHNvE/syBE+4CeUGw9dwNz8OHKQfd1SYIHa6JQmexqnJFTRxXsyBcFGI62J6Xz4Xdn3Io3TU5BAdA7rcdhsEhR3w7WufAxVGb8+RsCsWM7HyI9qtdmM0/l+NUQ2+IPaaE4c73fZ36f1+n6EoJf/tS9d+0bBMKDo0zecRoG0/tKRSb1YtyhRS4ZP3r0bM9EYSfNKizsYg5bQroJ53qwsrjFU3pwlrkpQ5Egy03IaCcPgS02IoAlbsRwFJDAlbRk6jXYluiURU7E52q1JzoVJf9ybRq7w==",
         },
     ]
 
@@ -32,7 +33,17 @@ class SansaRoadSolver(Solver):
         self.add_program_line(grid_color_connected(color="not gray", adj_type="line"))
 
         for (r, c, d, _), symbol_name in puzzle.symbol.items():
+            fail_false(
+                not (d == Direction.CENTER and symbol_name == "circle_SS__5"),
+                f"Gray circle cannot be placed in the center of ({r}, {c}).",
+            )
             target = 2 if d == Direction.TOP_LEFT else 1
+
+            if d == Direction.CENTER and symbol_name == "circle_SS__1":
+                self.add_program_line(f"not gray({r}, {c}).")
+
+            if d == Direction.CENTER and symbol_name == "circle_SS__2":
+                self.add_program_line(f"gray({r}, {c}).")
 
             if d in (Direction.TOP, Direction.LEFT, Direction.TOP_LEFT) and symbol_name == "circle_SS__1":
                 self.add_program_line(count_covering(("lt", target), (r, c), d, color="gray"))
