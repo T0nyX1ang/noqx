563c2cc3b97cab1472de9ba8ace9582ee56623e8
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2026-04-15 17:54:40 +0800

    feat: support successive subtraction/division in kenken

--- solver/kenken.py
+++ solver/kenken.py
@@ -38,6 +38,9 @@ class KenkenSolver(Solver):
         {
             "data": "m=edit&p=7VfNjts2EL77KRa8lgX0R0rWbZvu5rJ12u4WQSAsDO/GbYzacGqvi0KG7z330kfpA+RN+iT5hhz5byQqSRugh8IQ8XHmG4oz31Cy1r9sJquptjrXaaEjHeOXWKvTKIPBXxH/7mZP82l5oS83T2+WKwCtX1xf6x8n8/VUD6rY0+4H23pY1pe6fl5WKlZaJbhida/r78pt/U1Zj3R9C5fSFrYbT0oArw7wpfMTeuaNcQQ8Ak6BAV8BPkxW6/FzT/u2rGqjFd3lKxdLUC2Wv04V74Lmj8vFw4wMD5Mn5LJ+M3vLnvXm9fLnDXNxA7XYzJ9mj8v5ckVGsu10fXmWAO2EE0gPCRD0CRA6T4AzpAQeZ6vH+XR8c0jh7t9KYdiewg7ifI8kxmVF+fxwgLflFuOo3KokJ/o2/tLuEKOVVeSP4TG2cMtH7/70RTJ5Qobk79//YEORkaH4wk9tEtE02/tt4lYomgVs5ua24Rd8A54O/fLv/vLzPHLL+RbAzN+LZ5m/VTPz9+GZ37dlFV+5VK/dmLjxDjXQderGr90YudG48cZxrlCA2OB8GGwqQUdnyQGnwFmDM2BsreFkKeP0YLewW7Yb2PeY1mRscS/La1ricKzB+g3OgM0RzgxjAztjA2wZW4o9shvLfHvAKXDWYHD2a5IdveHWoTWZg6dFbBs7cM72nDDbCRdsLxDb4JxwszfgnDFxhoxz7LmxE6YWcxzgYVNP4jDOUas9Rj3zpp6EuZ5kLxgXqPMek535BdYZEob4L10LPHNj5kbrWiN3Y9Eco+7jtaccn7QUVaiUwfF3D0h/yv5Jg/bsdDeoqFH3PxT5U/H9oFJXr3+aXoyWq8VkjkfQaLN4mK6aOd4Cu4H6TbmrSvBOwcPw/zfDf/nNQEpF/e+H03fBReqe1Z+xaT/meFU1mtQWulLYFSqn3m7GkzESdbMjr3BDj2O3l+LIj4Y88QtCfk4QjEIwBAXanFMEh9pFkCQLDShZknZWFqZJ3nl9OolZB1EyTRdTUkX1A1wpRIDcokmA3SZPNz1pFSrAb5csENAhXiCiS8ZASKeggZhuaQNBAZEDUSG5A2FB4QNx4RboDkx7mqEzrq8pOsJ6W6M1qr89WoI+oUHS/gYRIb3tISL6WkME9DSF4H/c8yALtoBgh4QX5IDcgtstsqB+8MM86xJUEDtkFLx28QStVTLB+pD3qmmRR3CkKIIipBCMvv8a5qzswn9abOE+KnH94sTDRcV39ZmHcydHpehz+3TJI/dpnEt2hMOJsP13zGlky5/Hz/qJg++O6n7wHg==",
         },
+        {
+            "data": "m=edit&p=7VTvaxNNEP6ev6Ls5xVuf10u963W1i81fV8bKeUIJU1PG7xweklENuR/7zNzs0kVhYooCnJk8szs7OwzszO7+riZdbUOOteu0Jk2+KwttM1z/Gf8S99ksW7q8kgfb9b3bQeg9cXZmX47a1a1HlSmd5sOtnFUxmMdX5aVMkori59RUx3/L7fxVRnHOl5iSekctvPeyQKeHuAVrxM66Y0mAx4DO2DAa8D5ops39c157/hfWcWJVnTOc95NUC3bT7USHqTP2+Xtggy3szWyWd0vPsjKanPXvt+IL45Qy02zXszbpu3ISLadjsffT8EdUiDYp0Do6xQkx1+cwujbKexwPa+RxE1ZUT5vDvCy3EKOy62ynty35lnYYY9WuaJ1gxU/zGkp6yvkC3YssmxvKPh0UUNgb4MGYLV3z0Ub8aJouc04lGjO7jWcfM3MzlhalhNQ1tGxfMEyYxlYnrPPKfiabKQN6JQWLWjQ3sYIRqODV48tsBPsgEGTsdfGit3CTqQIO/h7ieMRJ0j8gPhhJLh4hIfAKAzjHHgoOACjCIxxVggHblQOxuD/BZY4FjETJv5W+FjaKzwt7E7sDvbE3yKOS3EIS3wH/k44O9i92D3sKV88EsYLf095CX/2l5ieck8+5C8xPXxSrRz2Jg4ONXHJH3gfH/XxEp9wkHshfy+1ojo48TE4y8pevneKj0a44nY4YelZ5twmQ5ZFmoAnTEY/BT/TkT9Cp6LLkQ/pPwVNB5U6vXtXH43bbjlr8C6MN8vbuks6HufdQH1W/KssnnmU79+L/Ye/2HRZ2e/ozsNcxotHg8lKmsxekdHcKzSb+z00nKyk6WQljScraT7jxV88oJilajp4AA==",
+        },
     ]
 
     def solve(self, puzzle: Puzzle) -> str:
@@ -60,18 +63,15 @@ class KenkenSolver(Solver):
                         if len(ar) == 1:
                             self.add_program_line(f":- not number({r}, {c}, {clue}).")
 
-                        if len(ar) == 2:
-                            r0, c0 = ar[0]
-                            r1, c1 = ar[1]
-                            self.add_program_line(
-                                f":- number({r0}, {c0}, N0), number({r1}, {c1}, N1), (N0 - N1 * {clue}) * (N1 - N0 * {clue}) * (N0 - N1 - {clue}) * (N1 - N0 - {clue}) * (N0 + N1 - {clue}) * (N0 * N1 - {clue}) != 0."
-                            )
-
-                        if len(ar) > 2:
+                        if len(ar) > 1:
                             self.add_program_line(area_product_aggregate(_id=i, src_cells=ar))
-                            self.add_program_line(
-                                f":- S = #sum {{ N, R, C : area({i}, R, C), number(R, C, N) }}, area_product({i}, {len(ar) - 1}, P), (S - {clue}) * (P - {clue}) != 0."
-                            )
+                            if clue >= len(ar) * n:  # must be product operator
+                                self.add_program_line(f":- not area_product({i}, {len(ar) - 1}, {clue}).")
+                                self.add_program_line(number_exclusion(clue, grid_size=n, _id=i))
+                            else:
+                                self.add_program_line(
+                                    f":- S = #sum {{ N, R, C : area({i}, R, C), number(R, C, N) }}, area_product({i}, {len(ar) - 1}, P), M = #max {{ N: area({i}, R, C), number(R, C, N) }}, number(_, _, M), (S - {clue}) * (P - {clue}) * (S + {clue} - 2 * M) * (P * {clue} - M * M) != 0."
+                                )
 
                     else:
                         fail_false(str.isdigit(clue[:-1]), f"Invalid clue at ({r}, {c}).")
@@ -79,16 +79,13 @@ class KenkenSolver(Solver):
                         operator = (
                             clue[-1].replace("x", "*").replace("\u00d7", "*").replace("\u2212", "-").replace("\u00f7", "/")
                         )  # normalize operators
-                        fail_false(operator in ("+", "*") or len(ar) == 2, f"Invalid clue at ({r}, {c}).")
 
                         if operator == "+":
                             self.add_program_line(f":- #sum {{ N, R, C : area({i}, R, C), number(R, C, N) }} != {num}.")
 
-                        if operator == "-" and len(ar) == 2:
-                            r0, c0 = ar[0]
-                            r1, c1 = ar[1]
+                        if operator == "-":
                             self.add_program_line(
-                                f":- number({r0}, {c0}, N0), number({r1}, {c1}, N1), (N0 - N1 - {num}) * (N1 - N0 - {num}) != 0."
+                                f":- S = #sum {{ N, R, C : area({i}, R, C), number(R, C, N) }}, M = #max {{ N: area({i}, R, C), number(R, C, N) }}, number(_, _, M), 2 * M != S + {num}."
                             )
 
                         if operator == "*":
@@ -96,11 +93,10 @@ class KenkenSolver(Solver):
                             self.add_program_line(f":- not area_product({i}, {len(ar) - 1}, {num}).")
                             self.add_program_line(number_exclusion(int(num), grid_size=n, _id=i))
 
-                        if operator == "/" and len(ar) == 2:
-                            r0, c0 = ar[0]
-                            r1, c1 = ar[1]
+                        if operator == "/":
+                            self.add_program_line(area_product_aggregate(_id=i, src_cells=ar))
                             self.add_program_line(
-                                f":- number({r0}, {c0}, N0), number({r1}, {c1}, N1), (N0 - N1 * {num}) * (N1 - N0 * {num}) != 0."
+                                f":- area_product({i}, {len(ar) - 1}, P), M = #max {{ N: area({i}, R, C), number(R, C, N) }}, number(_, _, M), M * M != P * {num}."
                             )
 
                 if Point(r, c, Direction.CENTER, "normal") in puzzle.text:
