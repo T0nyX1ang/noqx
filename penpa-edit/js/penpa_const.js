e4ba068240d0b87c059442600ee9ff640bbc3cc3
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2026-04-19 13:58:00 +0800

    chore(deps): update Penpa+ to 3.2.3

--- penpa-edit/js/penpa_const.js
+++ penpa-edit/js/penpa_const.js
@@ -18,5 +18,5 @@ const style_tag_cache = {
   constraints_settings_css: "link#constraints_settings_css",
 };
 
-const penpa_edit_hash = "aaa65640a869e75b840dbe81a5ef323fc8be3d08";
+const penpa_edit_hash = "4f912b6aeab87a105d15d5e42a622624d5e78d0f";
 const penpa_edit_prefix = `https://cdn.jsdelivr.net/gh/swaroopg92/penpa-edit@${penpa_edit_hash}/docs/`;
