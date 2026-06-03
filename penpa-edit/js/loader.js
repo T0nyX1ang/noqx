c268ba7f7a35f3da225d717c8c475f3a492e55dc
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2026-06-03 22:42:22 +0800

    chore(deps): update Penpa+ to 3.2.4
    * merged new puzzlink parser

--- penpa-edit/js/loader.js
+++ penpa-edit/js/loader.js
@@ -1,4 +1,4 @@
-const penpa_edit_hash = "aabb797ddd7a05bde93eba774522964c77132dcf";
+const penpa_edit_hash = "34e3fe97804e518288870b70d919e7e76ee18b4d";
 const remote_penpa_prefix = `https://cdn.jsdelivr.net/gh/swaroopg92/penpa-edit@${penpa_edit_hash}/docs/`;
 const local_penpa_prefix = `./requires/core/${penpa_edit_hash}/`;
 
