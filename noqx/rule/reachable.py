c5cc5bb913081e77900b6b396db3f79766ef5895
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2026-06-16 00:20:14 +0800

    perf: use `#count=0` to make `avoid_unknown_src` faster

--- noqx/rule/reachable.py
+++ noqx/rule/reachable.py
@@ -228,12 +228,11 @@ def avoid_unknown_src(color: Optional[str] = "black", main_type: str = "grid", a
     if color is None:
         validate_type(adj_type, ("edge",))
         tag = tag_encode("reachable", main_type, "src", "adj", adj_type)
-        return f":- grid(R, C), not {tag}(_, _, R, C)."
+        return f":- grid(R, C), #count {{ SR, SC: {tag}(SR, SC, R, C) }} = 0."
 
     validate_type(adj_type, (4, 8, "line", "line_directed"))
     tag = tag_encode("reachable", main_type, "src", "adj", adj_type, color)
-
-    return f":- grid(R, C), {color}(R, C), not {tag}(_, _, R, C)."
+    return f":- grid(R, C), {color}(R, C), #count {{ SR, SC: {tag}(SR, SC, R, C) }} = 0."
 
 
 def grid_branch_color_connected(color: Optional[str] = "black", adj_type: Union[int, str] = 4) -> str:
